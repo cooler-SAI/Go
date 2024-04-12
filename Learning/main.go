7e45179a312a53b85bdcbfd1eb4b2d230b96f9cb
cooler-SAI <cooler19860212@yahoo.com>
2024-04-12 22:51:23 +0300

    Go/Learning: refactor all project

--- Learning/main.go
+++ Learning/main.go
@@ -3,29 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Hi all Here! :-)")
-
-	var name = "Ander"
-	fmt.Println(name)
-
-	var (
-		secondName   = "Tom"
-		number       = 32
-		buildVersion = 0.24
-	)
-	fmt.Println(secondName, number, "is", buildVersion)
-
-	fmt.Println("")
-
-	var (
-		music, songNumber = "Metal", 25
-	)
-	release := fmt.Sprintf("Music style is %s. And number of this song style is %d",
-		music, songNumber)
-
-	fmt.Println(release)
-
-	fmt.Println("Another test one")
+	fmt.Println("Ok, Let's GO!")
 
 }
