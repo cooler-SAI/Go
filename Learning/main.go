17e318df5853fe1639b6695f2953bc5c08b8ed56
cooler-SAI <cooler19860212@yahoo.com>
2024-04-12 23:05:47 +0300

    Go/Learning: more tests here and there

--- Learning/main.go
+++ Learning/main.go
@@ -5,9 +5,17 @@ import (
 )
 
 func main() {
-	var hello string = "Hello World"
+	var hello = "Hello World"
 	fmt.Println(hello)
 
 	fmt.Printf("Type  %T Data is: %v\n\n", hello, hello)
+
+	var valueA = 256
+	var valueB = 345
+	var result = valueA * valueB
+
+	fmt.Printf("\n\n"+
+		"Result is: "+
+		"Main result is: %v\n\n", result)
 
 }
