bf6f305deb1dd0b661ca47eb6067e30bec541978
cooler-SAI <cooler19860212@yahoo.com>
2024-04-15 14:44:02 +0300

    Go/Learning: added build files

--- Learning/main.go
+++ Learning/main.go
@@ -1,19 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	result := 0
-
-	for i := 0; i < 10; i++ {
-		result += i
-	}
-	fmt.Println(result)
-
-	nextResult := 0
-	for nextResult < 5000 {
-		nextResult += 10
-	}
-	fmt.Println(nextResult)
+	fmt.Println("Hello World")
+	fmt.Println("Press any Key to exit....")
+	fmt.Scanln()
+	os.Exit(0)
 }
