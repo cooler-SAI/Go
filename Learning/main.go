eb4ce54722494924ce30bc2ad5d6cd5293bea552
cooler-SAI <cooler19860212@yahoo.com>
2024-04-15 17:02:46 +0300

    Go/Game: defer method

--- Learning/main.go
+++ Learning/main.go
@@ -1,18 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Man")
-	fmt.Println("Enter the Number: \n ")
-	var number int
-	fmt.Scanln(&number)
-
-	switch number {
-	case 1:
-		fmt.Println("Hello Man")
-	case 2:
-		fmt.Println("Hello Dude")
-	}
+	defer fmt.Println("world")
+	fmt.Println("hello world")
 
 }
