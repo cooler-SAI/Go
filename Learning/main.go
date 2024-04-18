d2b28130f30aa8ae8ccb11cbe26e0ceb24c28bab
cooler-SAI <cooler19860212@yahoo.com>
2024-04-18 13:52:31 +0300

    Go/Game: added slices

--- Learning/main.go
+++ Learning/main.go
@@ -4,8 +4,35 @@ import "fmt"
 
 func main() {
 
+	letters := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	fmt.Println(letters)
+	letters = append(letters, "a")
+	fmt.Println(letters)
+	letters = append(letters, "25352")
+	fmt.Println(letters)
+
+	createSlice := make([]string, 2)
+	createSlice[0] = "Meeee"
+	createSlice[1] = "YESS"
+	fmt.Println(len(createSlice))
+	fmt.Println(cap(createSlice))
+	createSlice = append(createSlice, "5")
+	fmt.Println(len(createSlice))
+	fmt.Println(cap(createSlice))
+	createSlice = append(createSlice, "6", "7", "8")
+	fmt.Println(len(createSlice))
+	fmt.Println(cap(createSlice))
+
+	fmt.Println(createSlice)
+}
+
+func massive() {
 	var a [2]string
 	a[0] = "Hello"
 	a[1] = "Man"
 	fmt.Println(a[0], a[1])
+
+	numbers := [...]int{1, 2, 3}
+	fmt.Println(numbers[0], numbers[1], numbers[2])
+	fmt.Println(numbers)
 }
