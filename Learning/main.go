da9aaf89321d6d0cf37c47ace6f31444fd764730
cooler-SAI <cooler19860212@yahoo.com>
2024-04-18 13:20:42 +0300

    Go/Game: added massive

--- Learning/main.go
+++ Learning/main.go
@@ -1,44 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
-type point struct {
-	X int
-	Y int
-	S string
-}
-
-func (p *point) test() {
-	fmt.Println(p.X, p.Y, p.S)
-}
+import "fmt"
 
 func main() {
-	structs()
-	p2 := point{
-		X: 1,
-		Y: 2,
-		S: "Andrey",
-	}
-	p2.test()
-	p3 := &p2
-	p3.test()
 
-	/*fmt.Scanln()*/
-
-}
-
-func structs() {
-	p1 := point{1, 2, "Hello"}
-	fmt.Println(p1)
-	fmt.Println(p1.X, p1.Y)
-	p1.X = 22416
-	fmt.Println(p1.X)
-
-	p2 := point{X: 1}
-	fmt.Println(p2)
-
-	g := &p1
-	fmt.Println(*g)
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "Man"
+	fmt.Println(a[0], a[1])
 }
