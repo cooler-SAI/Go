7af0afa4c07492b5016b6102b7b539f3849cd2b1
cooler-SAI <cooler19860212@yahoo.com>
2024-04-18 14:09:36 +0300

    Go/Game: added more slices

--- Learning/main.go
+++ Learning/main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 
-	animalsArray := [4]string{
-		"dog",
-		"cat",
-		"duck",
-		"wolf",
-	}
-	animalsSlice := animalsArray[0:2]
-	fmt.Println(animalsSlice)
+	test := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	slice := test[:5]
+	fmt.Println(slice)
+	slice = test[5:]
+	fmt.Println(slice)
+	slice = test[:]
+	fmt.Println(slice)
+
 }
