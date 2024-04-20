4a856bced0ea160425a64d76522a6791975be0a5
cooler-SAI <cooler19860212@yahoo.com>
2024-04-20 23:57:39 +0300

    Go/Learn: clearn up

--- Learning/main.go
+++ Learning/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func doSomething(callback func(int, int) int, s string) int {
 	result := callback(8, 12)
 	fmt.Println(result)
+	fmt.Println(s)
 	return result
 }
 func main() {
@@ -12,13 +13,13 @@ func main() {
 		return a + b
 	}
 
-	result := doSomething(sumCallback, "hello world")
+	result := doSomething(sumCallback, "sum result")
 	fmt.Println(result)
 
 	multipleCallBack := func(a, b int) int {
 		return a * b
 	}
-	result = doSomething(multipleCallBack, "hello world")
+	result = doSomething(multipleCallBack, "multiple call back")
 	fmt.Println(result)
 
 }
