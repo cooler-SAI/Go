b91591ffb62f2cbd2fad059711c26b412af223c1
cooler-SAI <cooler19860212@yahoo.com>
2024-04-25 20:34:17 +0300

    Go/Learn: update project

--- Learning/main.go
+++ Learning/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/mitchellh/mapstructure"
 )
 
 type Point struct {
-	X int
-	Y int
+	X int `mapstructure:"A"`
+	Y int `mapstructure:"B"`
 }
 
 func main() {
 	pointsMap := map[string]int{
-		"A": 1,
+		"A": 150,
 		"B": 2,
 	}
-	fmt.Println(pointsMap)
+
+	point1 := Point{}
+
+	check := mapstructure.Decode(pointsMap, &point1)
+	if check != nil {
+		return
+	}
+	fmt.Println(point1)
 }
