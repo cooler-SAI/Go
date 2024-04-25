2fc9df4edc7c412d64e59303630433bc8fb7a861
cooler-SAI <cooler19860212@yahoo.com>
2024-04-25 19:35:09 +0300

    Go/Learn: test with maps

--- Learning/main.go
+++ Learning/main.go
@@ -13,5 +13,9 @@ func main() {
 	pointsMap["A"] = Point{X: 1, Y: 2}
 	otherMap["B"] = 20
 	pointsMap["B"] = Point{X: 2, Y: 3}
+
 	fmt.Println(pointsMap)
+	fmt.Println(otherMap)
+
+	fmt.Println(pointsMap["A"])
 }
