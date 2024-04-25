5b53c249f616f9de93334c81ee07d9f1aa151441
cooler-SAI <cooler19860212@yahoo.com>
2024-04-25 19:00:41 +0300

    Go/Learn: set up arrays

--- Learning/main.go
+++ Learning/main.go
@@ -2,43 +2,12 @@ package main
 
 import "fmt"
 
-type Point struct {
-	X, Y int
-}
-
-func (point Point) movePoint(X, Y int) Point {
-	point.X += X
-	point.Y += Y
-	return point
-}
-
-func (point *Point) movePointPtr(X, Y int) {
-	point.X += X
-	point.Y += Y
-}
-
 func main() {
 
-	basePoint := Point{0, 0}
-	fmt.Println(basePoint)
-	fmt.Println(basePoint.movePoint(4, 5))
-	fmt.Println(basePoint.movePoint(20, 25))
+	array := []string{"a", "b", "c", "d", "e", "f"}
+	fmt.Println(array)
 
-	test := "Hello!"
-	defer fmt.Println(test)
-
-	fmt.Println(basePoint)
-
-	basePoint.movePointPtr(1, 1)
-	fmt.Println(basePoint)
-
-	nextPoint := &basePoint
-	nextPoint.movePointPtr(100, 100)
-	fmt.Println(basePoint)
-
-	basePoint.movePointPtr(200, 200)
-	fmt.Println(basePoint)
-	basePoint.movePoint(1000, 1000)
-	fmt.Println(basePoint)
-
+	for i, l := range array {
+		fmt.Println(i, l)
+	}
 }
