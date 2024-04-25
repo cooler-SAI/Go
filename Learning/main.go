858e3987dccd4f56aef455bda06eafdb75c157a8
cooler-SAI <cooler19860212@yahoo.com>
2024-04-25 16:03:11 +0300

    Go/Learn: added points, misc updates

--- Learning/main.go
+++ Learning/main.go
@@ -2,17 +2,31 @@ package main
 
 import "fmt"
 
-func totalPrice(initPrice int) func(int) int {
-	sum := initPrice
-	return func(x int) int {
-		sum *= x
-		return sum
-	}
+type Point struct {
+	X, Y int
+}
+
+func movePoint(point Point, X, Y int) Point {
+	point.X += X
+	point.Y += Y
+	return point
+}
+
+func movePointPtr(point *Point, X, Y int) {
+	point.X += X
+	point.Y += Y
 }
 
 func main() {
 
-	order := totalPrice(10)
-	result := order(5)
-	fmt.Println(result)
+	basePoint := Point{0, 0}
+	fmt.Println(basePoint)
+	fmt.Println(movePoint(basePoint, 4, 5))
+	fmt.Println(movePoint(basePoint, 20, 25))
+
+	fmt.Println(basePoint)
+
+	movePointPtr(&basePoint, 1, 1)
+	fmt.Println(basePoint)
+
 }
