20c00df1625a4c9143860a7c02d7d13889117dfb
cooler-SAI <cooler19860212@yahoo.com>
2024-05-02 22:03:50 +0300

    Go/Learn: update formats

--- Learning/main2.go
+++ Learning/main2.go
@@ -2,45 +2,14 @@ package main
 
 import "fmt"
 
-var _ User = &user{}
-
-type user struct {
-	Name, Surname, Email string
-}
-
-func (u *user) ChangeName(newName string) int {
-	//TODO implement me
-	u.Name = newName
-	return 0
-}
-
-func (u *user) ChangeSurname(newSurname string) {
-	//TODO implement me
-	u.Surname = newSurname
-}
-
-func (u *user) ChangeEmail(newEmail string) {
-	//TODO implement me
-	u.Email = newEmail
-}
-
-type User interface {
-	ChangeName(newName string) int
-	ChangeSurname(newSurname string)
-	ChangeEmail(newEmail string)
-}
-
-func NewUser(name string, surname string, email string) User {
-	return &user{
-		Name:    name,
-		Surname: surname,
-		Email:   email,
-	}
-}
-
 func main() {
+	var a interface {
+	}
 
-	ander := NewUser("Ander", "Ander", "ander@ander.net")
-	fmt.Println(ander)
-
+	a = "nice"
+	fmt.Println(a)
+	fmt.Printf("(%v,%T)\n\n", a, a)
+	a = 42
+	fmt.Println(a)
+	fmt.Printf("(%v,%T)\n\n", a, a)
 }
