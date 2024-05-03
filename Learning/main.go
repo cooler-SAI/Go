400ffcd8e48a63002984e1e2f45b8e76d898ceac
cooler-SAI <cooler19860212@yahoo.com>
2024-05-03 21:28:21 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,31 +1,5 @@
 package main
 
-import "fmt"
-
-type AppError struct {
-	Message string
-	Err     error
-	Field   int
-}
-
-func (e *AppError) Error() string {
-	return e.Err.Error()
-}
-
-func m() error {
-	return &AppError{
-		Err:     fmt.Errorf("my error"),
-		Message: "my bad",
-		Field:   25,
-	}
-}
-
 func main() {
-	err := m()
-	if err != nil {
-		fmt.Println(err.Error(), err.Error())
-	} else {
-		fmt.Println("Error")
-	}
 
 }
