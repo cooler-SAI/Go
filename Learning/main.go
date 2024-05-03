a294bf4c13d66b46139d635a37d2833f5e6c19aa
cooler-SAI <cooler19860212@yahoo.com>
2024-05-03 20:43:05 +0300

    Go/Learn: check numbers

--- Learning/main.go
+++ Learning/main.go
@@ -5,12 +5,17 @@ import (
 )
 
 func Read(number int) int {
-	fmt.Println("Press any key to continue...")
-	return number
+	check := number * 250
+	if check > 1000 {
+		fmt.Println("We good")
+
+	} else {
+		fmt.Println("We too bad")
+	}
+	return check
 }
 
 func main() {
-	Read(250)
-	fmt.Println(Read(250))
+	Read(100)
 
 }
