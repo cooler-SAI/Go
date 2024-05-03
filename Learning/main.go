5ce519b50477bda442e41a2f8a5c19c812324f13
cooler-SAI <cooler19860212@yahoo.com>
2024-05-03 21:36:42 +0300

    Go/Learn: added reader

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 
+	var reader = strings.NewReader("hello guys!")
+	array := make([]byte, 8)
+	fmt.Println(array)
+
+	err := fmt.Errorf("Error....")
+	fmt.Println(reader)
+	fmt.Println(err)
 }
