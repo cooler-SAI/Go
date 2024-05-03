3cc9f594e85d1801e02d816743ecb396a0000af3
cooler-SAI <cooler19860212@yahoo.com>
2024-05-03 20:40:01 +0300

    Go/Learn: clean up, new tests added

--- Learning/main.go
+++ Learning/main.go
@@ -2,49 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-type structNumber struct {
-	N1, N2 int
-}
-
-func (s *structNumber) Sum() int {
-	return s.N1 + s.N2
-}
-
-type InterfaceNumber interface {
-	Sum() int
-}
-
-type otherNumbers struct {
-	A, B int
-}
-
-func (o otherNumbers) Multiply() int {
-	return o.A * o.B
-}
-
-type baseNumbers struct {
-	A, B int
-}
-
-func (b baseNumbers) Sum() int {
-	return b.A + b.B
+func Read(number int) int {
+	fmt.Println("Press any key to continue...")
+	return number
 }
 
 func main() {
-	var _ InterfaceNumber
-	sn := &structNumber{1, 2}
-	fmt.Println(sn.Sum())
+	Read(250)
+	fmt.Println(Read(250))
 
-	on := otherNumbers{10, 20}
-	fmt.Println(on.Multiply())
-
-	bn := baseNumbers{10000, 20000}
-	fmt.Println(bn.Sum())
-
-	fmt.Println("Press Enter to Exit....")
-	fmt.Scanln()
-	os.Exit(0)
 }
