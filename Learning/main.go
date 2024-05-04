354889241f302da86d839a00895d0f406ad32ada
cooler-SAI <cooler19860212@yahoo.com>
2024-05-04 21:59:22 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-type name struct {
-	A int
-	B string
-}
-
-func (receiver name) String() string {
-
-	fmt.Println(receiver.B)
-	return receiver.String()
-
-}
-
-func (receiver *name) Numbers() int {
-	return receiver.A
-}
-
 func main() {
 
-	var reader = strings.NewReader("hello guys!")
-	array := make([]byte, 8)
-	_, _ = reader.Read(array) // Read operation to avoid unused variable warning
-	fmt.Println(string(array))
-
-	var err = fmt.Errorf("error")
-	fmt.Println(reader)
-	fmt.Println(err)
 }
