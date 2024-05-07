d14aa93a2abed72f60e08e2230cde844d0618b55
cooler-SAI <cooler19860212@yahoo.com>
2024-05-07 22:02:10 +0300

    Go/Learn: set up channel

--- Learning/main.go
+++ Learning/main.go
@@ -5,18 +5,24 @@ import (
 	"time"
 )
 
-func Say(word string) {
+func Say(word string, ch chan int) {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Hello", word)
+	ch <- 1
 }
+
 func main() {
 
-	go Say("Ander")
+	ch := make(chan int)
+
+	go Say("Ander", ch)
 	fmt.Println("1")
 	fmt.Println("2")
 	fmt.Println("3")
 	fmt.Println("4")
 	fmt.Println("5")
 	time.Sleep(10 * time.Second)
+
+	<-ch
 
 }
