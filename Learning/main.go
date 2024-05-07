aa060b38d9160da9b5aa1099e03c07f012592927
cooler-SAI <cooler19860212@yahoo.com>
2024-05-07 21:37:58 +0300

    Go/Learn: test simple

--- Learning/main.go
+++ Learning/main.go
@@ -1,27 +1,13 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
+func Say(word string) {
+	fmt.Println("Hello", word)
+}
 func main() {
+
 	fmt.Println("Hello World")
-	fmt.Println("Press any key...")
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		if scanner.Text() == "\x1b" {
-			fmt.Println("Exiting...")
-		} else {
-			fmt.Println("You pressed a key other than Escape.")
-			fmt.Println("Press Esc to exit...")
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+	Say("Ander")
 }
