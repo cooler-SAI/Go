0af27436a13a6dc12408f258e63eb112a4a29788
cooler-SAI <cooler19860212@yahoo.com>
2024-05-07 21:42:41 +0300

    Go/Learn: added go routine

--- Learning/main.go
+++ Learning/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Say(word string) {
+	time.Sleep(1 * time.Second)
 	fmt.Println("Hello", word)
 }
 func main() {
 
-	fmt.Println("Hello World")
+	go Say("Ander")
+	fmt.Println("1")
+	fmt.Println("2")
+	fmt.Println("3")
+	fmt.Println("4")
+	fmt.Println("5")
+	time.Sleep(10 * time.Second)
 
-	Say("Ander")
 }
