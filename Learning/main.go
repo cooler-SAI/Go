729ea1f3affdef828d712866d69ee4c5a8f8f6f9
cooler-SAI <cooler19860212@yahoo.com>
2024-05-08 14:43:34 +0300

    Go/Learn: fix deadlock

--- Learning/main.go
+++ Learning/main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"time"
 )
 
 func Say(word string) {
-	time.Sleep(8 * time.Second)
 	fmt.Println(word)
 }
 func sayHello(exit chan string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
 		Say("hello " + strconv.Itoa(i))
 
 	}
@@ -23,7 +20,6 @@ func main() {
 
 	ch := make(chan string)
 
-	sayHello(ch)
 	go sayHello(ch)
 	fmt.Println(<-ch)
 
