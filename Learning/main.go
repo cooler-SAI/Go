abb143c2301a2e74b2763e82d2801cd686024041
cooler-SAI <cooler19860212@yahoo.com>
2024-05-10 22:06:01 +0300

    Go/Learn: added sync wg

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 func main() {
+
+	var waitGroup sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		waitGroup.Add(2)
+		key := i
+		go func() {
+			defer waitGroup.Done()
+			fmt.Printf("%d Goroutine sleeping....\n", key)
+			time.Sleep(300 * time.Millisecond)
+		}()
+	}
 
 }
