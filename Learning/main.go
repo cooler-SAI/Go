9f78a2735a971ec82ed732ec0d471f80dadc5ccc
cooler-SAI <cooler19860212@yahoo.com>
2024-05-10 21:44:31 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-type Counter struct {
-	mu sync.RWMutex
-	c  map[string]int
-}
-
-func (c *Counter) CountMe() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.c["test"]++
-}
-
-func (c *Counter) CountMeAgain() map[string]int {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	copyMap := make(map[string]int, len(c.c))
-	for k, v := range c.c {
-		copyMap[k] = v
-	}
-	return copyMap
-}
-
 func main() {
-	c := Counter{c: make(map[string]int)}
-	for i := 0; i < 1000; i++ {
-		go c.CountMe()
-	}
 
-	time.Sleep(time.Second * 3)
-
-	fmt.Println(c.CountMeAgain())
 }
