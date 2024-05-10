63dfcb8752eb2a2f2cc40d51dd1f9282697e5cc3
cooler-SAI <cooler19860212@yahoo.com>
2024-05-10 22:19:52 +0300

    Go/Learn: test counter

--- Learning/main.go
+++ Learning/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
 )
 
 func main() {
@@ -17,7 +16,8 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for j := 0; j < 1000; j++ {
-				atomic.AddUint64(&counter, 1)
+				counter++
+				//atomic.AddUint64(&counter, 1)
 
 			}
 		}()
