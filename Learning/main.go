afcdeb0412a9745b9531758e84403b778e57485c
cooler-SAI <cooler19860212@yahoo.com>
2024-05-10 20:49:19 +0300

    Go/Learn: create base func, test goroutine

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "sync"
+
+type Counter struct {
+	mu sync.Mutex
+	c  map[string]int
+}
+
+func (c *Counter) increment(key string) {
+	c.c[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	return c.c[key]
+}
+
 func main() {
+
+	key := "test"
+
+	c := Counter{c: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.increment(key)
+	}
 
 }
