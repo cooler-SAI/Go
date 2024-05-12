b6ce74a7ab5fe81d1c8b3cfd0169f03f905e67e5
cooler-SAI <cooler19860212@yahoo.com>
2024-05-12 22:40:58 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var once sync.Once
-	var checker = 25
 
-	onceBody := func() {
-		once.Do(func() {
-
-			fmt.Printf("Number is: %d\n", checker)
-		})
-	}
-	fmt.Println("Before calling onceBody")
-	onceBody()
-	fmt.Println("After calling onceBody")
-
-	fmt.Println("Before calling onceBody again")
-	onceBody()
-	fmt.Println("After calling onceBody again")
 }
