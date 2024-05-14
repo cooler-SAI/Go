05608c34e3b51d9bb416e077ff51c83481d0b9e5
cooler-SAI <cooler19860212@yahoo.com>
2024-05-14 23:12:53 +0300

    Go/Tour: set base const

--- Tour/main.go
+++ Tour/main.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func testValue() {
-	i := 42
-	fmt.Printf("i is of type %T\n", i)
-	j := 526.32
-	fmt.Printf("j is of type %T\n", j)
-	s := "Hello"
-	fmt.Printf("s is of type %T\n", s)
+	const Pi = 3.14
+	fmt.Println(Pi)
+	const World = "World"
+	fmt.Println(World)
 }
 
 func main() {
