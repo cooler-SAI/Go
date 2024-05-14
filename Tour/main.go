1a481d078b5c4329445025be112bd8f637a4bdce
cooler-SAI <cooler19860212@yahoo.com>
2024-05-14 23:19:38 +0300

    Go/Tour: check for in func

--- Tour/main.go
+++ Tour/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func testValue() {
-	const Pi = 3.14
-	fmt.Println(Pi)
-	const World = "World"
-	fmt.Println(World)
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+		fmt.Printf("%d\t", sum)
+	}
 }
 
 func main() {
