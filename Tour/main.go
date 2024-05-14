affd2047c3bcbfc5f334ea00eb664c715ccb5cee
cooler-SAI <cooler19860212@yahoo.com>
2024-05-14 22:54:48 +0300

    Go/Tour: set base values

--- Tour/main.go
+++ Tour/main.go
@@ -1,16 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func testValue() {
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
-
+	i := 42
+	f := float64(i)
+	u := uint(f)
+	fmt.Println(i, f, u)
 }
 
 func main() {
