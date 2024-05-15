39f499dcb92304e7fd62870f3f82d3e73e61768b
cooler-SAI <cooler19860212@yahoo.com>
2024-05-15 22:59:06 +0300

    Go/Tour: added defer func

--- Tour/main.go
+++ Tour/main.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func testValue() {
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon!")
-	case t.Hour() < 19:
-		fmt.Println("Good evening!")
-	case t.Hour() < 20:
-		fmt.Println("Good night!")
-	default:
-		fmt.Println("Good noon!")
+	fmt.Println("hello guys!")
+}
 
-	}
+func testValueNext() {
+	fmt.Println("yes, you!")
 }
 
 func main() {
+	defer testValueNext()
 	testValue()
 
 }
