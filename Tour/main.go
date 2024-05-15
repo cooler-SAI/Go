12cfcba2be441e5adb9caf7f8748a340212cf08c
cooler-SAI <cooler19860212@yahoo.com>
2024-05-15 22:23:28 +0300

    Go/Tour: test different things

--- Tour/main.go
+++ Tour/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func testValue() {
-	sum := 1
-	for sum < 10 {
-		sum += sum
-
+	sum := 100
+	if sum < 100 {
+		fmt.Printf("%d\t", sum)
+	} else {
+		fmt.Printf("%d\t", sum)
 	}
+
 	fmt.Printf("%d\t", sum)
 }
 
