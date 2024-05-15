374eba3151ff4d5988aacc7abef28db1d99a64bb
cooler-SAI <cooler19860212@yahoo.com>
2024-05-15 22:57:13 +0300

    Go/Tour: set up default clock check

--- Tour/main.go
+++ Tour/main.go
@@ -6,20 +6,19 @@ import (
 )
 
 func testValue() {
-	fmt.Println("Where is Sunday ?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("today")
-
-	case today + 1:
-		fmt.Println("tomorrow")
-	case today + 2:
-		fmt.Println("quarter")
-	case today + 3:
-		fmt.Println("in 3 days")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon!")
+	case t.Hour() < 19:
+		fmt.Println("Good evening!")
+	case t.Hour() < 20:
+		fmt.Println("Good night!")
 	default:
-		fmt.Println("today")
+		fmt.Println("Good noon!")
+
 	}
 }
 
