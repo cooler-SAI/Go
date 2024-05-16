643d3b698c6137639600a2a80389d666080e776a
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 17:32:33 +0300

    Go/Tour: test slices of slices

--- Tour/main.go
+++ Tour/main.go
@@ -2,20 +2,14 @@ package main
 
 import "fmt"
 
-func printSlices(s string, x []int) {
-
-	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
-}
-
 func testSlices() {
 
-	a := make([]int, 10)
+	var slices [][]int
 
-	b := make([]int, 0, 10)
-	c := make([]int, 10)
-	printSlices("a:", a)
-	printSlices("b:", b)
-	printSlices("c:", c)
+	slices = append(slices, []int{1, 2, 3})
+	slices = append(slices, []int{4, 5, 6})
+	slices = append(slices, []int{7, 8, 9})
+	fmt.Println(slices)
 
 }
 
