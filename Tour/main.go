6d4671963d0c84460cabfabfcfc9aef5e549f28b
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 16:27:26 +0300

    Go/Tour: added base structs

--- Tour/main.go
+++ Tour/main.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-func testValue() {
-	i, j := 20, 100
-	p := &i
-	fmt.Println(*p)
-	*p = 500
-	fmt.Println(i)
-	p = &j
-	*p = *p / 2
-	fmt.Println(j)
+type Values struct {
+	X, Y int
+}
+
+func testStruct() {
+	fmt.Println(Values{1, 2})
 
 }
 
 func main() {
-	testValue()
+	testStruct()
+
 }
