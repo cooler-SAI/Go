9df87acc873798350ff4f69ed2ba03a20717421d
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 17:38:15 +0300

    Go/Tour: test continue range

--- Tour/main.go
+++ Tour/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func testRange() {
-	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
-	for i, v := range pow {
-		fmt.Printf("2**%d = %d\n", i, v)
+	pow := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		pow[i] = 1 << uint(i)
+
+	}
+	for _, v := range pow {
+		fmt.Println(v)
 	}
 
 }
