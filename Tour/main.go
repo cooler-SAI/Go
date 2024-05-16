1196fce647f6d78892a896906285c6cb24d2edff
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 17:34:54 +0300

    Go/Tour: added range

--- Tour/main.go
+++ Tour/main.go
@@ -2,18 +2,15 @@ package main
 
 import "fmt"
 
-func testSlices() {
-
-	var slices [][]int
-
-	slices = append(slices, []int{1, 2, 3})
-	slices = append(slices, []int{4, 5, 6})
-	slices = append(slices, []int{7, 8, 9})
-	fmt.Println(slices)
+func testRange() {
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 
 }
 
 func main() {
-	testSlices()
+	testRange()
 
 }
