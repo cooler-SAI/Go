c73632bfffd0ada2cffed45ac2e7c25640255025
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 20:54:24 +0300

    Go/Tour: add maps mutating

--- Tour/main.go
+++ Tour/main.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m = map[string]Vertex{
-	"Bell Labs": {
-		40.68433, -74.39967,
-	},
-	"Google": {
-		37.42202, -122.08408,
-	},
+func mutatingMaps() {
+	m := make(map[string]int)
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
 
 func main() {
 
-	fmt.Println(m)
+	mutatingMaps()
 }
