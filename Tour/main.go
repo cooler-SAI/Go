d880e0e615dfa93849134044ca30329db4731cd5
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 16:20:08 +0300

    Go/Tour: clean up

--- Tour/main.go
+++ Tour/main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
-var lastValue int
-
 func testValue() {
-	fmt.Println("counting....")
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		lastValue = i
-	}
-}
 
-func result() {
-	fmt.Println("The last value of i was:", lastValue)
 }
 
 func main() {
-	defer result()
 	testValue()
 }
