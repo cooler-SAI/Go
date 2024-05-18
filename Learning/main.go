663799bdc74dead2e328550a9ea38c76e551b8a0
cooler-SAI <cooler19860212@yahoo.com>
2024-05-18 21:31:19 +0300

    Go/Learn: set up base type

--- Learning/main.go
+++ Learning/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 
-	helloGo := "Hello Go!"
-	continueWords := "You are damn right"
-	date := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Println(helloGo + " " + continueWords)
-	fmt.Println("Current date and time now is " + " " + date)
+	var name = "Tom"
+	fmt.Printf("Hello, %s!\n"+"var type is %T\n", name, name)
 
 }
