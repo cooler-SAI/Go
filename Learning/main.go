f2b38e664ce73df246395e235b2192638f765e93
cooler-SAI <cooler19860212@yahoo.com>
2024-05-18 21:33:26 +0300

    Go/Learn: added const

--- Learning/main.go
+++ Learning/main.go
@@ -7,6 +7,9 @@ import (
 func main() {
 
 	var name = "Tom"
+	const pi = 3.1415926
 	fmt.Printf("Hello, %s!\n"+"var type is %T\n", name, name)
+	fmt.Printf("Type is %T\n", pi)
+	fmt.Println(pi)
 
 }
