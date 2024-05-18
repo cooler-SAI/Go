0c008e8c18da3324c4fa3cc9d48db860f27ba753
cooler-SAI <cooler19860212@yahoo.com>
2024-05-18 21:38:29 +0300

    Go/Learn: more multi added

--- Learning/main.go
+++ Learning/main.go
@@ -11,9 +11,16 @@ func main() {
 		pi      = 3.1415926
 		nuclear = 1000
 	)
+
+	var (
+		mainframe = "Andrey"
+		surname   = "Shatohin"
+	)
 	fmt.Printf("Hello, %s!\n"+"var type is %T\n", name, name)
 	fmt.Printf("Type is %T\n", pi)
 	fmt.Println(pi)
 	fmt.Println(nuclear)
+	fmt.Println(surname)
+	fmt.Println(mainframe)
 
 }
