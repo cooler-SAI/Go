dfe60e53473f936400ac906a919c31ecfa3b9bd8
cooler-SAI <cooler19860212@yahoo.com>
2024-05-18 21:40:18 +0300

    Go/Learn: base func

--- Learning/main.go
+++ Learning/main.go
@@ -1,26 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
-	var name = "Tom"
-	const (
-		pi      = 3.1415926
-		nuclear = 1000
-	)
-
 	var (
-		mainframe = "Andrey"
-		surname   = "Shatohin"
+		a = 200
+		b = 300
+		c = a + b
 	)
-	fmt.Printf("Hello, %s!\n"+"var type is %T\n", name, name)
-	fmt.Printf("Type is %T\n", pi)
-	fmt.Println(pi)
-	fmt.Println(nuclear)
-	fmt.Println(surname)
-	fmt.Println(mainframe)
+	fmt.Println(c)
 
 }
