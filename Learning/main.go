0a93a63620f9d4e7fdf79abd8c55b3d7d0872770
cooler-SAI <cooler19860212@yahoo.com>
2024-05-18 21:42:29 +0300

    Go/Learn: added math base

--- Learning/main.go
+++ Learning/main.go
@@ -10,5 +10,18 @@ func main() {
 		c = a + b
 	)
 	fmt.Println(c)
+	var (
+		d = 200
+		e = 300
+		f = d - e
+	)
+	fmt.Println(f)
+
+	var (
+		g = 1000
+		h = 2000
+		i = g * h
+	)
+	fmt.Println(i)
 
 }
