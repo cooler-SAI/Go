e52eef3792c8a25c3a737d1a087a9fbf6f9f0fc3
cooler-SAI <cooler19860212@yahoo.com>
2024-05-19 20:30:22 +0300

    Go/Learn: operands added

--- Learning/main.go
+++ Learning/main.go
@@ -4,24 +4,13 @@ import "fmt"
 
 func main() {
 
-	var (
-		a = 200
-		b = 300
-		c = a + b
-	)
+	a := 10
+	b := 20
+	c := a == b
 	fmt.Println(c)
-	var (
-		d = 200
-		e = 300
-		f = d - e
-	)
-	fmt.Println(f)
-
-	var (
-		g = 1000
-		h = 2000
-		i = g * h
-	)
-	fmt.Println(i)
+	c = a > b
+	fmt.Println(c)
+	c = a < b
+	fmt.Println(c)
 
 }
