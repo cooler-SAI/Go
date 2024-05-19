be1af42379b073851e40730cf19fda38113a77d2
cooler-SAI <cooler19860212@yahoo.com>
2024-05-19 20:33:16 +0300

    Go/Learn: array add

--- Learning/main.go
+++ Learning/main.go
@@ -4,13 +4,7 @@ import "fmt"
 
 func main() {
 
-	a := 10
-	b := 20
-	c := a == b
-	fmt.Println(c)
-	c = a > b
-	fmt.Println(c)
-	c = a < b
-	fmt.Println(c)
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
 
 }
