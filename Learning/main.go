61faa6b6501cedff65832076dfdae5418da1dd56
cooler-SAI <cooler19860212@yahoo.com>
2024-05-19 21:39:00 +0300

    Go/Learn: simplified

--- Learning/main.go
+++ Learning/main.go
@@ -6,11 +6,6 @@ func main() {
 
 	a := 100
 	b := 250
-	if a > b {
-		fmt.Printf("%d is greater than %d\n", a, b)
-
-	} else {
-		fmt.Printf("%d is less than %d\n", a, b)
-	}
+	fmt.Printf("%d is less than %d\n", a, b)
 
 }
