31832a70f936d853b6102246ea5782dd5fbaa222
cooler-SAI <cooler19860212@yahoo.com>
2024-05-20 20:05:44 +0300

    Go/Learn: added for

--- Learning/main.go
+++ Learning/main.go
@@ -4,14 +4,8 @@ import "fmt"
 
 func main() {
 
-	a := 8
-	switch a {
-	case 1:
-		fmt.Printf("%d is 1\n", a)
-	case 8:
-		fmt.Printf("%d is 1\n", a)
-	default:
-		fmt.Printf("%d is 0\n", a)
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
 
 	}
 
