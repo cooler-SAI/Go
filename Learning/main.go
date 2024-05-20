45b19a086efff28cb10fc84d5ea314f69b91e3e1
cooler-SAI <cooler19860212@yahoo.com>
2024-05-20 21:39:15 +0300

    Go/Learn: added anonymous func

--- Learning/main.go
+++ Learning/main.go
@@ -2,45 +2,8 @@ package main
 
 import "fmt"
 
-func simple() {
-	fmt.Println("Hello Golang")
-}
-
-func math(x, y int) {
-	z := x * y
-	fmt.Println("z:", z)
-}
-
-func floatfunc(x, y float64) float64 {
-	z := x * y
-	return z
-}
-
-func mathInt(x, y int) int {
-	return x + y
-}
-
-func resultMath(x, y float64) float64 {
-	z := x * y
-	return z
-}
-
-func add(x, y int, firstName, LastName string) (int, string) {
-	return x + y, firstName + " " + LastName
-}
-
 func main() {
-	simple()
-	math(1, 2)
-	result := floatfunc(1.5, 2.8)
-	fmt.Println(result)
-	mathResult := mathInt(1, 2)
-	fmt.Println(mathResult)
-	multiResult := resultMath(1.5, 2.8)
-	fmt.Println(multiResult)
-
-	var age, name = add(4, 5, "ander", "me")
-	fmt.Println(age)
-	fmt.Printf(name)
+	f := func(x, y int) int { return y*x + y }
+	fmt.Println(f(3, 4))
 
 }
