6d653f378d029f68c05c866a0bae73c0d32a02d3
cooler-SAI <cooler19860212@yahoo.com>
2024-05-22 21:11:15 +0300

    Go/Learn: added point

--- Learning/main.go
+++ Learning/main.go
@@ -4,25 +4,9 @@ import "fmt"
 
 func main() {
 
-	var people = map[string]int{
-		"James": 32,
-		"Jose":  12,
-		"Sam":   20,
-	}
-	fmt.Println(people["James"])
-
-	var numbers = map[int]int{150: 32, 350: 12, 250: 20}
-	fmt.Println(numbers[350])
-
-	for k, v := range numbers {
-		fmt.Println(k, v)
-
-	}
-
-	numbers[1000] = 3500
-	fmt.Println(numbers)
-
-	numbers[99999] = 1
-	fmt.Println(numbers)
+	x := 10
+	var p *int
+	p = &x
+	fmt.Println(p)
 
 }
