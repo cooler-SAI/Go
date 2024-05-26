f14e55e24218df11498edd1ae321ab12336dc205
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 18:50:05 +0300

    Go/Learn: added method struct,more files

--- Learning/main.go
+++ Learning/main.go
@@ -21,9 +21,19 @@ func (p person) eat(meal string) {
 	fmt.Println(p.name, "eat", meal)
 }
 
+func (c contact) print() {
+	fmt.Println("email:", c.email)
+	fmt.Println("phone:", c.phone)
+}
+
 func main() {
 	var tom = person{name: "Tom", age: 20}
+	var tomAddress = contact{
+		email: "tom@tom",
+		phone: "+123456789",
+	}
 	tom.print()
 	tom.eat("hamburger and cola")
+	tomAddress.print()
 
 }
