e063dc0ae6d3db27e47ec6ac2a798885b6bb1557
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 19:06:57 +0300

    Go/Learn: added method struct,more files, links

--- Learning/main.go
+++ Learning/main.go
@@ -2,38 +2,24 @@ package main
 
 import "fmt"
 
-type contact struct {
-	email string
-	phone string
-}
-
 type person struct {
 	name string
 	age  int
 }
 
-func (p person) print() {
-	fmt.Println("name:", p.name)
-	fmt.Println("age:", p.age)
-}
-
-func (p person) eat(meal string) {
-	fmt.Println(p.name, "eat", meal)
-}
-
-func (c contact) print() {
-	fmt.Println("email:", c.email)
-	fmt.Println("phone:", c.phone)
+func (p *person) updateAge(newAge int) {
+	(*p).age = newAge
 }
 
 func main() {
-	var tom = person{name: "Tom", age: 20}
-	var tomAddress = contact{
-		email: "tom@tom",
-		phone: "+123456789",
+
+	var tom = person{
+		name: "Tom",
+		age:  25,
 	}
-	tom.print()
-	tom.eat("hamburger and cola")
-	tomAddress.print()
+	var tomPointer = &tom
+	fmt.Println("Before: ", tom.age)
+	tomPointer.updateAge(30)
+	fmt.Println("After: ", tom.age)
 
 }
