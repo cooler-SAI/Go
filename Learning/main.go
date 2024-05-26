1ff71203f52662ed7cf522a770e82dbdf0aabec7
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 18:45:55 +0300

    Go/Learn: added method struct

--- Learning/main.go
+++ Learning/main.go
@@ -10,34 +10,20 @@ type contact struct {
 type person struct {
 	name string
 	age  int
-	contact
+}
+
+func (p person) print() {
+	fmt.Println("name:", p.name)
+	fmt.Println("age:", p.age)
+}
+
+func (p person) eat(meal string) {
+	fmt.Println(p.name, "eat", meal)
 }
 
 func main() {
-
-	var tom = person{"Tom", 20, contact{
-		email: "tom@tom",
-		phone: "+123456789",
-	}}
-	fmt.Println("New person added: ", tom.name, tom.age)
-	tom.age = 26
-	fmt.Println("Old person added: ", tom.name, tom.age)
-	tom.name = "Tom Super"
-	fmt.Println("Old person added: ", tom.name, tom.age)
-
-	var tomPointer = &tom
-	tomPointer.name = "Tom Super Shadow"
-	fmt.Println("Old person added: ", tomPointer.name, tomPointer.age)
-
-	ander := person{"Ander", 20, contact{
-		email: "ander@ander",
-		phone: "+123456789",
-	}}
-	var agePointer = &tom.age
-	*agePointer = 38
-	fmt.Println("Ander person added: ", ander.name, ander.age)
-
-	tomPointer.email = "newEmail@newEmail.com"
-	fmt.Println("New person added: ", tomPointer.name, tomPointer.age)
+	var tom = person{name: "Tom", age: 20}
+	tom.print()
+	tom.eat("hamburger and cola")
 
 }
