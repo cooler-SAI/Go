e6a7f5af07ea73b606b6502d8c1d919e90c104a7
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 16:01:50 +0300

    Go/Learn: added edited structs

--- Learning/main.go
+++ Learning/main.go
@@ -11,5 +11,9 @@ func main() {
 
 	var tom = person{"Tom", 20}
 	fmt.Println("New person added: ", tom.name, tom.age)
+	tom.age = 26
+	fmt.Println("Old person added: ", tom.name, tom.age)
+	tom.name = "Tom Super"
+	fmt.Println("Old person added: ", tom.name, tom.age)
 
 }
