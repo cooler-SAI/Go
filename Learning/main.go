16087acc811fb5787fbe76a86ebe562327289d09
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 16:58:47 +0300

    Go/Learn: added edited structs,more added

--- Learning/main.go
+++ Learning/main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email string
+	phone string
+}
+
 type person struct {
 	name string
 	age  int
+	contact
 }
 
 func main() {
 
-	var tom = person{"Tom", 20}
+	var tom = person{"Tom", 20, contact{
+		email: "tom@tom",
+		phone: "+123456789",
+	}}
 	fmt.Println("New person added: ", tom.name, tom.age)
 	tom.age = 26
 	fmt.Println("Old person added: ", tom.name, tom.age)
@@ -20,9 +29,15 @@ func main() {
 	tomPointer.name = "Tom Super Shadow"
 	fmt.Println("Old person added: ", tomPointer.name, tomPointer.age)
 
-	ander := person{"Ander", 20}
+	ander := person{"Ander", 20, contact{
+		email: "ander@ander",
+		phone: "+123456789",
+	}}
 	var agePointer = &tom.age
 	*agePointer = 38
 	fmt.Println("Ander person added: ", ander.name, ander.age)
+
+	tomPointer.email = "newEmail@newEmail.com"
+	fmt.Println("New person added: ", tomPointer.name, tomPointer.age)
 
 }
