bc7a5675bc970cd742405dae2bdc0081972b1124
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 15:59:44 +0300

    Go/Learn: added structs

--- Learning/main.go
+++ Learning/main.go
@@ -2,17 +2,14 @@ package main
 
 import "fmt"
 
-type distance = uint
-type kilometer = float64
-
-func realDistance(p, q kilometer) distance {
-	return distance(p*p + q*q)
-
+type person struct {
+	name string
+	age  int
 }
 
 func main() {
 
-	realDistance(25, 35)
-	fmt.Println(realDistance(25, 35))
+	var tom = person{"Tom", 20}
+	fmt.Println("New person added: ", tom.name, tom.age)
 
 }
