1ce6206639affdda088447c5bb281bd61b5c170b
cooler-SAI <cooler19860212@yahoo.com>
2024-05-26 14:54:45 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
-func change(x int) *int {
-	p := new(int)
-	*p = x
-	return p
-}
-
 func main() {
-
-	p1 := change(100)
-	fmt.Println(*p1)
-	p2 := change(1000)
-	fmt.Println(*p2)
-	p3 := change(10000)
-	fmt.Println(*p3)
 
 }
