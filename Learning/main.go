928e199154a79090f54f85cb54e79c8a2cf8292c
cooler-SAI <cooler19860212@yahoo.com>
2024-05-27 10:59:20 +0300

    Go/Learn: clean up, base added

--- Learning/main.go
+++ Learning/main.go
@@ -2,24 +2,8 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
-}
-
-func (p *person) updateAge(newAge int) {
-	(*p).age = newAge
-}
-
 func main() {
 
-	var tom = person{
-		name: "Tom",
-		age:  25,
-	}
-	var tomPointer = &tom
-	fmt.Println("Before: ", tom.age)
-	tomPointer.updateAge(30)
-	fmt.Println("After: ", tom.age)
+	fmt.Println("Hello Go")
 
 }
