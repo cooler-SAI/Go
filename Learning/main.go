4da1cf99e60d16a12a98f62ea24cfa61324dc999
cooler-SAI <cooler19860212@yahoo.com>
2024-05-27 11:01:32 +0300

    Go/Learn: packages test, again

--- Learning/main.go
+++ Learning/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
 func main() {
 
 	fmt.Println("Hello Go")
+	fmt.Println(math.Sqrt(200))
+	fmt.Println(math.Sqrt(-100))
+	fmt.Println("Press any key to exit")
+	fmt.Scanln()
+	os.Exit(0)
 
 }
