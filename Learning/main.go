f4689a72b53d1e8579664021323bd76d1ffcdbd7
cooler-SAI <cooler19860212@yahoo.com>
2024-05-27 13:52:53 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,11 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"main.go/computation"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Hello Go")
-	fmt.Println("Result all package is: ", computation.ResultNumberNew(111111))
+
 }
