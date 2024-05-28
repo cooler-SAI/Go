d12b3be217ff94ef45d8702df06553547dbcc0fa
cooler-SAI <cooler19860212@yahoo.com>
2024-05-28 13:20:27 +0300

    Go/Learn: added new struct

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+type Vehicle interface {
+	move()
+}
+type Moto struct {
+	name string
+}
+type Bicycle struct {
+	name string
+}
+
+func (m Moto) move() {
+	fmt.Printf("%s moved to %s\n", m.name, m.name)
+}
+func (b Bicycle) move() {
+	fmt.Printf("%s moved to %s", b.name, b.name)
+}
+
 func main() {
+	var X36512 Vehicle = Moto{"X36512"}
+	X36512.move()
+	var V12 Vehicle = Bicycle{"V12"}
+	V12.move()
 
 }
