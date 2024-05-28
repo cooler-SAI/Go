3d1d7bd324c346e4cdfdf414940a677937e10277
cooler-SAI <cooler19860212@yahoo.com>
2024-05-28 13:08:23 +0300

    Go/Learn: added more interfaces

--- Learning/main.go
+++ Learning/main.go
@@ -1,9 +1,32 @@
 package main
 
-type carMove interface {
+import "fmt"
+
+type Vehicle interface {
 	move()
 }
 
+type Car struct {
+	model string
+}
+
+type Aircraft struct {
+	model string
+}
+
+func (car Car) move() {
+	fmt.Println("Car move")
+}
+func (aircraft Aircraft) move() {
+	fmt.Println("Aircraft flying")
+}
+
 func main() {
+	var tesla Vehicle = Car{model: "X80"}
+	tesla.move()
+	var bowing Vehicle = Aircraft{model: "B4513"}
+	bowing.move()
+	fmt.Println(tesla)
+	fmt.Println(bowing)
 
 }
