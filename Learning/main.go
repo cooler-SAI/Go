7ecab8b69745c3f3ac15731a4b72290bf0d49756
cooler-SAI <cooler19860212@yahoo.com>
2024-05-29 21:49:50 +0300

    Go/Learn: set up new goroutines

--- Learning/main.go
+++ Learning/main.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
+var counter int = 0
+
 func main() {
 
-	intChan := make(chan int, 3)
-	intChan <- 10
-	intChan <- 20
-	close(intChan)
-	fmt.Println(<-intChan)
-	fmt.Println(<-intChan)
-	fmt.Println(<-intChan)
+	ch := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go work(i, ch)
 
+	}
+	for i := 0; i < 10; i++ {
+		<-ch
+	}
+	fmt.Println("The End")
+
+}
+
+func work(i int, ch chan bool) {
+	counter = 0
+	for k := 1; k <= 10; k++ {
+		counter++
+		fmt.Printf("Goroutine: %d\n", counter)
+	}
+	ch <- true
 }
