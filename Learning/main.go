7c67e7c8ea76c07b66fd1401736148bb2f62c582
cooler-SAI <cooler19860212@yahoo.com>
2024-05-29 21:00:27 +0300

    Go/Learn: added goroutine

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	for i := 1; i < 7; i++ {
+		go factorial(i)
+	}
+	fmt.Scanln()
+	fmt.Println(" The End")
+
+}
+
+func factorial(n int) {
+	if n < 1 {
+		fmt.Println("Error: The factorial function must have a positive number")
+		return
+	}
+	result := 1
+	for i := 1; i < n; i++ {
+		result = result * result
+
+	}
+	fmt.Printf("The factorial of %d is %d\n", n, result)
 }
