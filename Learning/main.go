b042b9424b1d908ee7800ddf3ae0b06f0a75609e
cooler-SAI <cooler19860212@yahoo.com>
2024-05-29 21:12:23 +0300

    Go/Learn: set up channels

--- Learning/main.go
+++ Learning/main.go
@@ -4,16 +4,20 @@ import "fmt"
 
 func main() {
 
-	for i := 1; i < 7; i++ {
-		go func(n int) {
-			result := 1
-			for j := 1; j <= n; j++ {
-				result *= j
-			}
-			fmt.Println(n, "-", result)
-		}(i)
-	}
-	fmt.Scanln()
+	intChan := make(chan int)
+
+	go factorial(5, intChan)
+	fmt.Println(<-intChan)
 	fmt.Println("The End")
 
+}
+
+func factorial(n int, intChan chan int) {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	fmt.Println(n, "-", result)
+
+	intChan <- result
 }
