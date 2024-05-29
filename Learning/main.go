1f66ab9c0b13abadcaed5585d5ac2a946f751880
cooler-SAI <cooler19860212@yahoo.com>
2024-05-29 20:08:59 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,31 +1,5 @@
 package main
 
-import "fmt"
-
-type Vehicle interface {
-	move()
-}
-type Moto struct {
-	name string
-}
-type Bicycle struct {
-	name string
-}
-
-func (m Moto) move() {
-	fmt.Printf("%s moved to %s\n", m.name, m.name)
-}
-func (b Bicycle) move() {
-	fmt.Printf("%s moved to %s", b.name, b.name)
-}
-
 func main() {
-	var X36512 Vehicle = Moto{"X36512"}
-	var V12 Vehicle = Bicycle{"V12"}
-
-	vehicles := []Vehicle{X36512, V12}
-	for _, vehicle := range vehicles {
-		vehicle.move()
-	}
 
 }
