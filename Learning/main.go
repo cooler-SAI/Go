6fe2a289161f9be56b18a5b8afa7c43ccb9e7e5f
cooler-SAI <cooler19860212@yahoo.com>
2024-05-29 21:04:52 +0300

    Go/Learn: added goroutine anonim func

--- Learning/main.go
+++ Learning/main.go
@@ -5,22 +5,15 @@ import "fmt"
 func main() {
 
 	for i := 1; i < 7; i++ {
-		go factorial(i)
+		go func(n int) {
+			result := 1
+			for j := 1; j <= n; j++ {
+				result *= j
+			}
+			fmt.Println(n, "-", result)
+		}(i)
 	}
 	fmt.Scanln()
-	fmt.Println(" The End")
+	fmt.Println("The End")
 
-}
-
-func factorial(n int) {
-	if n < 1 {
-		fmt.Println("Error: The factorial function must have a positive number")
-		return
-	}
-	result := 1
-	for i := 1; i < n; i++ {
-		result = result * result
-
-	}
-	fmt.Printf("The factorial of %d is %d\n", n, result)
 }
