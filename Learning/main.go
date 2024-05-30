675caea7664687c9ff7bbb9e54947fc51fce75e1
cooler-SAI <cooler19860212@yahoo.com>
2024-05-30 21:47:54 +0300

    Go/Learn: set up io.Reader

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"io"
+)
+
+type phoneReader string
+
+func (ph phoneReader) Read(p []byte) (int, error) {
+	count := 0
+	for i := 0; i < len(ph); i++ {
+		if ph[i] >= '0' && ph[i] <= '9' {
+			p[count] = ph[i]
+			count++
+		}
+	}
+	return count, io.EOF
+}
 func main() {
+
+	phone1 := phoneReader("+3-632-78-124-20")
+	phone2 := phoneReader("+2-52-345-856-19")
+
+	buffer := make([]byte, len(phone1))
+	phone1.Read(buffer)
+	fmt.Println(string(buffer))
+
+	buffer = make([]byte, len(phone2))
+	phone2.Read(buffer)
+	fmt.Println(string(buffer))
 
 }
