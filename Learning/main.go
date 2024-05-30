23b0fa0c15522d2e757d44dd11c8dbdba7638bc6
cooler-SAI <cooler19860212@yahoo.com>
2024-05-30 21:38:56 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
 func main() {
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	work := func(id int) {
-		defer wg.Done()
-		fmt.Printf("Worker %d\n", id)
-		time.Sleep(2 * time.Second)
-		fmt.Printf("Worker %d\n", id)
-	}
-
-	go work(1)
-	go work(2)
-
-	wg.Wait()
-	fmt.Println("Done")
 
 }
