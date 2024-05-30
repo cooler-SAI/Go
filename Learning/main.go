9170b174d666bc2ba13252cba58bd120f166b083
cooler-SAI <cooler19860212@yahoo.com>
2024-05-30 21:55:12 +0300

    Go/Learn: set up io.Reader and io.Writer

--- Learning/main.go
+++ Learning/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 type phoneReader string
+type phoneWriter struct {
+}
 
 func (ph phoneReader) Read(p []byte) (int, error) {
 	count := 0
@@ -17,6 +19,21 @@ func (ph phoneReader) Read(p []byte) (int, error) {
 	}
 	return count, io.EOF
 }
+
+func (ph phoneWriter) Write(bs []byte) (int, error) {
+	if len(bs) == 0 {
+		return 0, nil
+	}
+	for i := 0; i < len(bs); i++ {
+		if bs[i] >= '0' && bs[i] <= '9' {
+			fmt.Print(string(bs[i]))
+
+		}
+	}
+	fmt.Println()
+	return len(bs), nil
+}
+
 func main() {
 
 	phone1 := phoneReader("+3-632-78-124-20")
@@ -29,5 +46,12 @@ func main() {
 	buffer = make([]byte, len(phone2))
 	phone2.Read(buffer)
 	fmt.Println(string(buffer))
+
+	bytes1 := []byte("+123456789")
+	bytes2 := []byte("+987654321")
+
+	writer := phoneWriter{}
+	writer.Write(bytes1)
+	writer.Write(bytes2)
 
 }
