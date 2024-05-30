3519d637923b5568bfef95119e01922e86c47f66
cooler-SAI <cooler19860212@yahoo.com>
2024-05-30 23:16:10 +0300

    Go/Learn: add base

--- Learning/main.go
+++ Learning/main.go
@@ -1,57 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"io"
-)
-
-type phoneReader string
-type phoneWriter struct {
-}
-
-func (ph phoneReader) Read(p []byte) (int, error) {
-	count := 0
-	for i := 0; i < len(ph); i++ {
-		if ph[i] >= '0' && ph[i] <= '9' {
-			p[count] = ph[i]
-			count++
-		}
-	}
-	return count, io.EOF
-}
-
-func (ph phoneWriter) Write(bs []byte) (int, error) {
-	if len(bs) == 0 {
-		return 0, nil
-	}
-	for i := 0; i < len(bs); i++ {
-		if bs[i] >= '0' && bs[i] <= '9' {
-			fmt.Print(string(bs[i]))
-
-		}
-	}
-	fmt.Println()
-	return len(bs), nil
-}
+import "fmt"
 
 func main() {
 
-	phone1 := phoneReader("+3-632-78-124-20")
-	phone2 := phoneReader("+2-52-345-856-19")
+	var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
 
-	buffer := make([]byte, len(phone1))
-	phone1.Read(buffer)
-	fmt.Println(string(buffer))
-
-	buffer = make([]byte, len(phone2))
-	phone2.Read(buffer)
-	fmt.Println(string(buffer))
-
-	bytes1 := []byte("+123456789")
-	bytes2 := []byte("+987654321")
-
-	writer := phoneWriter{}
-	writer.Write(bytes1)
-	writer.Write(bytes2)
+	fmt.Print(cars)
+	fmt.Println(cars)
 
 }
