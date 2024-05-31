600483beb1b3db0c1ad84b670082a436a973082e
cooler-SAI <cooler19860212@yahoo.com>
2024-05-31 21:35:11 +0300

    Go/Learn: looping

--- Learning/main.go
+++ Learning/main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"}
-
-	fmt.Print(cars)
-	fmt.Println(cars)
-
+	for i := 0; i < 6; i++ {
+		fmt.Println(i)
+	}
 }
