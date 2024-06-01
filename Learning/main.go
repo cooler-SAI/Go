050b0243b5d4a2b4dc952f82619f320800e4b848
cooler-SAI <cooler19860212@yahoo.com>
2024-06-01 12:31:00 +0300

    Go/Learn: added base func

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
+type car struct {
+	color string
+	make  string
+}
+
+func driving(c car) string {
+	return fmt.Sprintf("%s %s", c.color, c.make)
+}
 func main() {
+
+	var toyota = car{
+		color: "White",
+		make:  "Sprinter",
+	}
+	driving(toyota)
+	fmt.Println(driving(toyota))
 
 }
