e7b1ac3a2a990dae639d209779396d9e640459fa
cooler-SAI <cooler19860212@yahoo.com>
2024-06-01 13:05:50 +0300

    Go/Learn: added diff models

---
+++ Learning/cars/cars_models.go
@@ -0,0 +1,21 @@
+package cars
+
+type Car struct {
+	Color string
+	Make  string
+}
+
+var Toyota = Car{
+	Color: "White",
+	Make:  "Lite Ace",
+}
+
+var Jac = Car{
+	Color: "Brown",
+	Make:  "BX70",
+}
+
+var Nissan = Car{
+	Color: "White",
+	Make:  "Pathfinder",
+}
--- Learning/main.go
+++ Learning/main.go
@@ -1,22 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"main.go/cars"
+)
 
-type car struct {
-	color string
-	make  string
+type carLocal struct {
+	colorLocal string
+	makeLocal  string
 }
 
-func driving(c car) string {
-	return fmt.Sprintf("%s %s", c.color, c.make)
+func driving(c cars.Car) string {
+	return fmt.Sprintf(`%s %s`, c.Color, c.Make)
 }
+
+func drivingLocal(c carLocal) string {
+	return fmt.Sprintf(`%s %s`, c.colorLocal, c.makeLocal)
+}
+
 func main() {
 
-	var toyota = car{
-		color: "White",
-		make:  "Sprinter",
+	var toyota = carLocal{
+		colorLocal: "White",
+		makeLocal:  "Sprinter",
 	}
-	driving(toyota)
-	fmt.Println(driving(toyota))
+	drivingLocal(toyota)
+	fmt.Println(drivingLocal(toyota))
+	fmt.Printf("Now new Server models: \n")
+
+	var drivingResults []string
+
+	drivingResults = append(drivingResults, driving(cars.Jac))
+	drivingResults = append(drivingResults, driving(cars.Nissan))
+	drivingResults = append(drivingResults, driving(cars.Toyota))
+	drivingResults = append([]string{""}, drivingResults...)
+
+	for _, result := range drivingResults {
+		fmt.Println(result)
+	}
 
 }
