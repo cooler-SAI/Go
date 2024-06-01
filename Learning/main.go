dba226f46b4c920e09ad94e1102bcf71692d36d2
cooler-SAI <cooler19860212@yahoo.com>
2024-06-01 13:22:40 +0300

    Go/Learn: added base nil

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	var a, b, c = 10, 20, 30
+	d := a + b
+	if false {
+		fmt.Println("100", d)
+	} else {
+		err := errors.New("error")
+		fmt.Println(nil, err)
+	}
+
+	fmt.Println(a, b, c)
 }
