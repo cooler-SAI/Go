c91d0bc6a5400752b419fa046a1b81a5883f78cc
cooler-SAI <cooler19860212@yahoo.com>
2024-06-02 14:13:28 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,37 +1,5 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-type errorString struct {
-	s string
-}
-
-func (e *errorString) Error() string {
-	return e.s
-}
-
 func main() {
-	err2 := fmt.Errorf("math: square root of negative number ")
-	fmt.Println(err2)
-
-	fmt.Println(errorString{"error here"})
-	err := &errorString{"error here"}
-	fmt.Println(*err)
-	var x, y = 20, 35
-	z := x + y
-	if false {
-		panic(err)
-
-	} else {
-		fmt.Println(z)
-	}
-	baseError := errors.New("base error here")
-	if baseError != nil {
-		fmt.Println(baseError)
-
-	}
 
 }
