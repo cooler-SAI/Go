9c52dc29af05bc53ecb38db4890d8cfef9c41280
cooler-SAI <cooler19860212@yahoo.com>
2024-06-02 14:13:28 +0300

    Go/Learn: add base http

--- Learning/main.go
+++ Learning/main.go
@@ -1,37 +1,48 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"html/template"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 )
 
-type errorString struct {
-	s string
+var viewTemplate = template.Must(template.New("view").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Record</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    <p>{{.Content}}</p>
+</body>
+</html>
+`))
+
+type Record struct {
+	Title   string
+	Content string
 }
 
-func (e *errorString) Error() string {
-	return e.s
+func init() {
+	http.HandleFunc("/view", viewRecord)
+}
+
+func viewRecord(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	key := datastore.NewKey(c, "Record", r.FormValue("id"), 0, nil)
+	record := new(Record)
+	if err := datastore.Get(c, key, record); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := viewTemplate.Execute(w, record); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	err2 := fmt.Errorf("math: square root of negative number ")
-	fmt.Println(err2)
-
-	fmt.Println(errorString{"error here"})
-	err := &errorString{"error here"}
-	fmt.Println(*err)
-	var x, y = 20, 35
-	z := x + y
-	if false {
-		panic(err)
-
-	} else {
-		fmt.Println(z)
-	}
-	baseError := errors.New("base error here")
-	if baseError != nil {
-		fmt.Println(baseError)
-
-	}
-
+	appengine.Main()
 }
