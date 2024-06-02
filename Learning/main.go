093439ed9f004012285360a6d09c7aa50310836d
cooler-SAI <cooler19860212@yahoo.com>
2024-06-02 14:47:09 +0300

    Go/Learn: set up new error

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+func devide(a, b int) (i int, int error) {
+	if a < b {
+		return 0, fmt.Errorf("can't devide")
+	}
+	return a / b, nil
+}
+
 func main() {
+
+	devide(20, 40)
+	fmt.Println(devide(10, 20))
 
 }
