a1739b7e33501d58a226c629586dc4da3b2e6e91
cooler-SAI <cooler19860212@yahoo.com>
2024-06-02 14:49:05 +0300

    Go/Learn: set up new error more

--- Learning/main.go
+++ Learning/main.go
@@ -9,9 +9,18 @@ func devide(a, b int) (i int, int error) {
 	return a / b, nil
 }
 
+func doSomething(a int, b int) (int, error) {
+	return a + b, fmt.Errorf("can't doSomething")
+
+}
+
 func main() {
 
-	devide(20, 40)
+	i, err := devide(20, 40)
+	if err != nil {
+		return
+	}
 	fmt.Println(devide(10, 20))
+	fmt.Println(doSomething(10, 20))
 
 }
