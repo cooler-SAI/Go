55fdb57911e7937aa8dffa72de9458c4c8916d85
cooler-SAI <cooler19860212@yahoo.com>
2024-06-03 15:09:40 +0300

    Go/Learn: set use package

--- Learning/main.go
+++ Learning/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"main.go/cars"
+)
 import "main.go/math"
+
+func modelsCache(carsModel []cars.Car) {
+	for _, car := range carsModel {
+		fmt.Println(car)
+	}
+}
 
 func main() {
 	xs := []float64{1, 2, 3, 4}
 	avg := math.Average(xs)
 	fmt.Println(avg)
+
+	carModels := []cars.Car{
+		cars.Toyota,
+		cars.Jac,
+		cars.Nissan,
+	}
+
+	modelsCache(carModels)
 }
