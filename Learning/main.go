54623f36768e5e4c33fc4fb0b8a7df74e8ed212d
cooler-SAI <cooler19860212@yahoo.com>
2024-06-03 14:42:56 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
-func panicHere() {
-	panic("panic here")
-}
-
-func test() {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("Recovered from panic:", err)
-		}
-	}()
-	panicHere()
-}
-
 func main() {
-
-	test()
-	fmt.Println("Program continues after recovery.")
 
 }
