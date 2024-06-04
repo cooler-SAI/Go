4922e4e5d455026767144431506cff0e3216b593
cooler-SAI <cooler19860212@yahoo.com>
2024-06-04 21:41:48 +0300

    Go/Learn: added simple slices

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	numbers := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+	var slice []int = numbers[1:4]
+	fmt.Println(slice)
+	slice2 := numbers[:2]
+	fmt.Println(slice2)
+	slice3 := numbers[1:3]
+	fmt.Println(slice3)
+	slice4 := numbers[1:]
+	fmt.Println(slice4)
 }
