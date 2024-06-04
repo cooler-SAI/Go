867aa5579f2e1e9d9cb89dcb0610524c0755a0b1
cooler-SAI <cooler19860212@yahoo.com>
2024-06-04 21:57:25 +0300

    Go/Learn: append new elements

--- Learning/main.go
+++ Learning/main.go
@@ -6,12 +6,20 @@ func main() {
 
 	numbers := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(numbers)
-	var slice []int = numbers[1:4]
+	var slice = numbers[1:4]
 	fmt.Println(slice)
 	slice2 := numbers[:2]
 	fmt.Println(slice2)
 	slice3 := numbers[1:3]
 	fmt.Println(slice3)
 	slice4 := numbers[1:]
+	fmt.Println(slice4)
+	fmt.Println(cap(slice4))
+	fmt.Println(len(slice4))
+
+	slice4[0] = 100
+	fmt.Println(slice4)
+
+	slice4 = append(slice4, 250, 450)
 	fmt.Println(slice4)
 }
