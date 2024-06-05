68a1ae2d4684aac051533789edb634133e5b4a07
cooler-SAI <cooler19860212@yahoo.com>
2024-06-05 21:33:17 +0300

    Go/Learn: added simple map

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var myMap map[string]int
+	myMap = make(map[string]int)
+	myMap["age"] = 30
+	myMap["score"] = 120
+	myMap["temp"] = 500
+	fmt.Println(myMap)
 }
