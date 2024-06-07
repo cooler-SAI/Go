c64716a91f5f1acab6a99e7f2c6f46ed625852ea
cooler-SAI <cooler19860212@yahoo.com>
2024-06-07 21:12:04 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,43 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	var myMap map[string]int
-	myMap = make(map[string]int)
-	myMap["age"] = 30
-	myMap["score"] = 120
-	myMap["temp"] = 500
-	fmt.Println(myMap)
-
-	newMap := map[string]int{
-		"a": 1,
-		"b": 2,
-		"c": 3,
-	}
-	fmt.Println(newMap)
-
-	Age := myMap["age"]
-	fmt.Println(Age)
-
-	for key, value := range newMap {
-		fmt.Println(key, value)
-	}
-
-	for key, value := range myMap {
-		fmt.Println(key, value)
-	}
-
-	value, ok := myMap["age"]
-	if ok {
-		fmt.Println(value)
-	} else {
-		fmt.Println("Value not found")
-	}
-
-	mySlice := []int{1, 2, 3}
-	fmt.Println(mySlice)
-	mySlice = append(mySlice, 4)
-	fmt.Println(mySlice)
 }
