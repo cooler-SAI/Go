f4ade6c715353ceab5dd7cf681d8a1f493ca2aa8
cooler-SAI <cooler19860212@yahoo.com>
2024-06-07 22:22:31 +0300

    Go/Learn: struct here

--- Learning/main.go
+++ Learning/main.go
@@ -2,28 +2,17 @@ package main
 
 import "fmt"
 
+type Car struct {
+	model string
+	year  int
+}
+
 func main() {
 
-	mySlice := make([]int, 3, 5)
-	fmt.Println("Slice: ", mySlice)
-	fmt.Println("Capability: ", cap(mySlice))
-	fmt.Println("Length", len(mySlice))
+	Toyota := Car{
+		model: "Corolla",
+		year:  1997,
+	}
+	fmt.Println(Toyota)
 
-	nextSlice := mySlice[:5]
-	fmt.Println("Slice: ", nextSlice)
-
-	fixedSlice := make([]int, 10)
-	fmt.Println("SliceNext: ", fixedSlice)
-
-	myMap := make(map[string]int)
-	myMap["Anna"] = 25
-	myMap["Bob"] = 30
-	fmt.Println("Map: ", myMap)
-
-	myChannel := make(chan int, 2)
-	myChannel <- 10
-	myChannel <- 20
-
-	fmt.Println("Channel Value 1 : ", <-myChannel)
-	fmt.Println("Channel Value 2 : ", <-myChannel)
 }
