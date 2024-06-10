0ebf0197351b00017f139c5b06c63303f7287ef0
cooler-SAI <cooler19860212@yahoo.com>
2024-06-10 21:08:09 +0300

    Go/Learn: clean

--- Learning/main.go
+++ Learning/main.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
-func do(i interface{}) {
-	switch i.(type) {
-	case int:
-		fmt.Println(i.(int))
-	case string:
-		fmt.Println(i.(string))
-	case bool:
-		fmt.Println(i.(bool))
-	default:
-		fmt.Println(i)
-	}
-}
-
 func main() {
-
-	var s = "Hello World"
-	do(s)
-
-	do(true)
-
-	do("Hello World")
 
 }
