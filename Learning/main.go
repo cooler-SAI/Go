875e09231f5e4d6b870552b323bd11cb0be30ddc
cooler-SAI <cooler19860212@yahoo.com>
2024-06-10 20:48:34 +0300

    Go/Learn: added types

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var i interface{} = "hello"
+	s := i.(string)
+	fmt.Println(s)
+
+	if v, ok := i.(string); ok {
+		fmt.Println(v)
+	}
+
+	str := i.(float64)
+	fmt.Println(str)
 
 }
