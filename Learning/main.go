eda2a5be83549852d00012c0a80dc82b6112d3ec
cooler-SAI <cooler19860212@yahoo.com>
2024-06-10 21:10:12 +0300

    Go/Learn: added String in types

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.name, p.age)
+}
+
 func main() {
+
+	a := Person{"Alex", 20}
+	b := Person{"Bob", 36}
+	fmt.Println(a, b, a)
 
 }
