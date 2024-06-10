68ad7692b4e4ec7f0893acf704aac54b3bda8986
cooler-SAI <cooler19860212@yahoo.com>
2024-06-10 20:30:33 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,18 +1,5 @@
 package main
 
-import "fmt"
-
-type Car struct {
-	model string
-	year  int
-}
-
 func main() {
-
-	Toyota := Car{
-		model: "Corolla",
-		year:  1997,
-	}
-	fmt.Println(Toyota)
 
 }
