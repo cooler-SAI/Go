bcd82bb107e5df1fb7b05766c933b33c7ecffc05
cooler-SAI <cooler19860212@yahoo.com>
2024-06-10 20:58:57 +0300

    Go/Learn: added types switches

--- Learning/main.go
+++ Learning/main.go
@@ -2,29 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-
-	var i interface{} = "hello"
-	s := i.(string)
-	fmt.Println(s)
-
-	if v, ok := i.(string); ok {
-		fmt.Println(v)
-	}
-
-	f, ok := i.(float64)
-	if ok {
-		fmt.Println(f)
-	} else {
+func do(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println(i.(int))
+	case string:
+		fmt.Println(i.(string))
+	case bool:
+		fmt.Println(i.(bool))
+	default:
 		fmt.Println(i)
 	}
+}
 
-	str, ok := i.(float64)
-	if ok {
-		fmt.Println(str)
-	} else {
-		err := fmt.Errorf("error Here")
-		fmt.Println(err)
-	}
+func main() {
+
+	var s = "Hello World"
+	do(s)
+
+	do(true)
+
+	do("Hello World")
 
 }
