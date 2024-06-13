1273c920fe85836b7f26a3db04bd3b1c5ed382f1
cooler-SAI <cooler19860212@yahoo.com>
2024-06-13 14:15:35 +0300

    Go/Learn: set up more interfaces

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,58 @@
 package main
 
+import "fmt"
+
+type Greeter interface {
+	Greet() string
+}
+
+type Gopher struct {
+	name string
+	age  int
+}
+
+func (g Gopher) Greet() string {
+	return "Hello " + g.name
+}
+
+func SayGreet(g Greeter) {
+	fmt.Println(g.Greet())
+}
+
+type Car struct {
+	Model string
+	Year  int
+}
+
+type Going interface {
+	Go() string
+}
+
+func (c Car) Go() string {
+	return "Riding.... " + c.Model + "!"
+}
+
+func Moving(g Going) {
+	fmt.Println(g.Go())
+}
+
 func main() {
+
+	p := Gopher{
+		name: "Ander",
+		age:  38,
+	}
+	fmt.Println(p.Greet())
+	SayGreet(p)
+
+	t := Car{
+		Model: "Toyota",
+		Year:  1987,
+	}
+	fmt.Println(t.Go())
+	Moving(t)
+
+	e := &p
+	fmt.Println(e.Greet())
 
 }
