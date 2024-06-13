9fa52906bb017319c22963ddbd2da4086ae25b37
cooler-SAI <cooler19860212@yahoo.com>
2024-06-13 19:44:52 +0300

    Go/Learn: added simple home devices

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,27 @@
 package main
 
+type Plug interface {
+	Working() string
+}
+
+type HomeDevice struct {
+	Name  string
+	Power int
+}
+
+func (h HomeDevice) Working() string {
+	return h.Name
+}
+
+func SetUp(plug Plug) string {
+	return plug.Working()
+}
+
 func main() {
+
+	t := HomeDevice{
+		Name:  "Toaster",
+		Power: 10,
+	}
 
 }
