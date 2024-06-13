1e0648f06e2422c3f478d556d7c6cf002e405f07
cooler-SAI <cooler19860212@yahoo.com>
2024-06-13 13:10:36 +0300

    Go/Learn: added simple interface

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+type Greeter interface {
+	Greet() string
+}
+
+type Gopher struct {
+	name string
+	age  int
+}
+
+func (g Gopher) Greet() string {
+	return "Hello " + g.name
+}
+
+func SayGreet(g Greeter) {
+	fmt.Println(g.Greet())
+}
+
 func main() {
+
+	p := Gopher{
+		name: "Ander",
+		age:  38,
+	}
+	fmt.Println(p.Greet())
+	SayGreet(p)
 
 }
