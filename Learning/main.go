872f4471d2445badc0b12a1a1f7ad4f0b0a29269
cooler-SAI <cooler19860212@yahoo.com>
2024-06-14 21:51:59 +0300

    Go/Learn: set up context, again

--- Learning/main.go
+++ Learning/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
+
+func doSomething(ctx context.Context) {
+	fmt.Println("do something")
+}
 
 func main() {
 
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+
+	ctx = context.WithValue(ctx, "key", "value")
+	go doSomething(ctx)
 
 }
