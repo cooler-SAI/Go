c2f3aa80e9e79de4c4f8d4d64131ed9575b0a191
cooler-SAI <cooler19860212@yahoo.com>
2024-06-15 10:55:35 +0300

    Go/Learn: added simple context exam

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func sleepAndTalk(ctx context.Context, d time.Duration, msg string) {
+	select {
+	case <-time.After(d):
+		fmt.Println(msg)
+	case <-ctx.Done():
+		fmt.Println("context cancelled")
+	}
+}
+
 func main() {
+
+	ctx := context.Background()
+	sleepAndTalk(ctx, 5*time.Second, "hello")
 
 }
