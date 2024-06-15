f533dd0cbb3433950745b8df05a69a25d8c3297d
cooler-SAI <cooler19860212@yahoo.com>
2024-06-15 18:45:57 +0300

    Go/Learn: added new func

--- Learning/main.go
+++ Learning/main.go
@@ -8,6 +8,12 @@ import (
 func testContext(ctx context.Context) context.Context {
 	return context.WithValue(ctx, "request ID", "12345")
 }
+
+func doSomething(ctx context.Context) {
+	rID := ctx.Value("request ID")
+	fmt.Println("request ID:", rID)
+}
+
 func main() {
 
 	fmt.Println("Start....")
