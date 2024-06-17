5f547323371ad3718dc318704bb491e9d4330db1
cooler-SAI <cooler19860212@yahoo.com>
2024-06-17 23:31:58 +0300

    Go/Learn: another goroutine

--- Learning/main.go
+++ Learning/main.go
@@ -7,14 +7,12 @@ import (
 
 func sayExample(s string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
 		fmt.Println(s, i)
 	}
 }
 
 func count(s string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
 		fmt.Println(s, i)
 	}
 }
@@ -23,12 +21,6 @@ func main() {
 	go sayExample("hello")
 	sayExample("world")
 
-	count("ander")
-	go count("here")
-
-	go func(s string) {
-		fmt.Println(s)
-	}("going")
 	time.Sleep(time.Second)
 	fmt.Println("done")
 }
