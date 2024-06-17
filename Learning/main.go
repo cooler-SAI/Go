c9f1cf8c6470a3ed54f96fe56f81404a56efecd1
cooler-SAI <cooler19860212@yahoo.com>
2024-06-17 23:29:40 +0300

    Go/Learn: add goroutine, update again

--- Learning/main.go
+++ Learning/main.go
@@ -11,10 +11,20 @@ func sayExample(s string) {
 		fmt.Println(s, i)
 	}
 }
+
+func count(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s, i)
+	}
+}
 func main() {
 
 	go sayExample("hello")
 	sayExample("world")
+
+	count("ander")
+	go count("here")
 
 	go func(s string) {
 		fmt.Println(s)
