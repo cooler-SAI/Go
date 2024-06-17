8d1bacd6833457052b42b93640dd70e124dd9666
cooler-SAI <cooler19860212@yahoo.com>
2024-06-17 22:45:39 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-)
-
-func testContext(ctx context.Context) context.Context {
-	return context.WithValue(ctx, "request ID", "12345")
-}
-
-func doSomething(ctx context.Context) {
-	rID := ctx.Value("request ID")
-	fmt.Println("request ID:", rID)
-}
-
 func main() {
 
-	fmt.Println("Start....")
-
-	ctx := context.Background()
-
-	ctx = testContext(ctx)
-
-	doSomething(ctx)
-
-	fmt.Println("End.", ctx, fmt.Errorf("error"))
 }
