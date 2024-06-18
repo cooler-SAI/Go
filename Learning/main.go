e55feb1694748790253a74bcc9d547107b63eab3
cooler-SAI <cooler19860212@yahoo.com>
2024-06-18 12:38:57 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func sayExample(s string, c chan int) {
-	for i := 0; i < 5; i++ {
-		c <- i
-		fmt.Println(s, i)
-	}
-}
-
 func main() {
 
-	go sayExample("hello", make(chan int))
-	sayExample("world", make(chan int))
-	c := make(chan int)
-	msg <- c
-	fmt.Println()
-	fmt.Println("done")
 }
