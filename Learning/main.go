7f4504334ca98ad9047a78e91d513f493c93a9b7
cooler-SAI <cooler19860212@yahoo.com>
2024-06-18 12:49:04 +0300

    Go/Learn: set up Buff chan

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	ch := make(chan int, 10)
+	fmt.Println("Base info:", ch)
+	ch <- 50
+	ch <- 200
+	fmt.Println("First value:", <-ch)
+	fmt.Println("Second info:", <-ch)
+	fmt.Println("Program Finished")
 
 }
