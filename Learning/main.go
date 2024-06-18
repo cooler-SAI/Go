c39939303f4473e7dc1ef8b7e1dbedbb864f85b3
cooler-SAI <cooler19860212@yahoo.com>
2024-06-18 12:56:54 +0300

    Go/Learn: add buffer

--- Learning/main.go
+++ Learning/main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
 
-	file, err := os.Open("test.txt")
-	if err != nil {
-		fmt.Println("Error Open file:", file)
-		return
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
-
-	reader := bufio.NewReader(file)
-
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error read line:", line)
-
-	}
-	fmt.Println("Reading....:", line)
-
+	var b bytes.Buffer
+	b.WriteString("Hello World")
+	fmt.Println(b.String())
+	b.WriteTo(os.Stdout)
 }
