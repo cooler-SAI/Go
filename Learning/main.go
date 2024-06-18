25977a1e92f3b509822f46afecce4cd44a8adf49
cooler-SAI <cooler19860212@yahoo.com>
2024-06-18 21:53:39 +0300

    Go/Learn: clean up again...

--- Learning/main.go
+++ Learning/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"os"
 )
 
 func main() {
 
-	var byteString bytes.Buffer
-	byteString.Write([]byte("Hello"))
-	_, _ = fmt.Fprintf(&byteString, "World")
-	byteString.WriteTo(os.Stdout)
+	fmt.Println("Hello World")
 }
