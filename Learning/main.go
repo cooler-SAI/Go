fa763ac538045f95f70ced67f0288d94956e61f8
SPP User <spp-user@spp-forum.de>
2024-06-20 22:43:15 +0300

    Go/Learn: added select in channels, again

--- Learning/main.go
+++ Learning/main.go
@@ -6,25 +6,25 @@ import (
 )
 
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
 	go func() {
 		time.Sleep(1 * time.Second)
-		ch1 <- "Data from ch1"
+		ch1 <- 1
 	}()
 
 	go func() {
 		time.Sleep(2 * time.Second)
-		ch2 <- "Data from ch2"
+		ch2 <- 2
 	}()
 
 	select {
-	case msg1 := <-ch1:
-		fmt.Println("Received message from ch1:", msg1)
-	case msg2 := <-ch2:
-		fmt.Println("Received message from ch2:", msg2)
+	case <-ch1:
+		fmt.Println("Add Data from chanel 1")
+	case <-ch2:
+		fmt.Println("Add Data from chanel 2")
 	default:
-		fmt.Println("Timeout: No messages received within 2 seconds")
+		fmt.Println("No data added")
 	}
 }
