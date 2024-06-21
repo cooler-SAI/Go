cb4730eaaedad6d4e8efeffcba94a43d70940821
cooler-SAI <cooler19860212@yahoo.com>
2024-06-21 15:26:35 +0300

    Go/Learn: test simple goroutine

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int
+
 func main() {
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func() {
+			counter++
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter: ", counter)
 
 }
