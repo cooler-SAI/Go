2e3bef33aa6eaa29145d30399bdfab53df19c354
cooler-SAI <cooler19860212@yahoo.com>
2024-06-21 15:37:26 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-var (
-	counter int
-	mutex   sync.Mutex
-)
-
 func main() {
-
-	var wg sync.WaitGroup
-	for i := 0; i < 10000; i++ {
-		wg.Add(1)
-		go func() {
-			mutex.Lock()
-			counter++
-			mutex.Unlock()
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-	fmt.Println("Counter: ", counter)
 
 }
