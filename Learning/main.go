0ee3090b0242eec1a2cc1aef1d0ad6d17bfd4d79
cooler-SAI <cooler19860212@yahoo.com>
2024-06-21 15:40:40 +0300

    Go/Learn: added scheduler

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
 func main() {
 
+	runtime.GOMAXPROCS(2)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			fmt.Println("Goroutine 1:", i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			fmt.Println("Goroutine 2:", i)
+		}
+	}()
+
+	wg.Wait()
 }
