c171abd50513607186f3cdd5c3e1e7b099d55760
coolerSAI <cooler19860212@yahoo.com>
2024-06-24 17:59:58 +0300

    Go/Learn: add generics, updated

--- Learning/main.go
+++ Learning/main.go
@@ -13,8 +13,16 @@ func AddType[T constraints.Ordered](a T, b T) T {
 	return a + b
 }
 
+func GMin[T constraints.Ordered](x, y T) T {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main() {
 	fmt.Println(Add(1, 2))
 	fmt.Println(AddType(1, 2.40))
 	fmt.Println(AddType("apple ", "banana"))
+	fmt.Println(GMin(1, 2))
 }
