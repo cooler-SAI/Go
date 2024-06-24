80268fea66eb2336a3bf6d632d07a4fb6d9ad91f
coolerSAI <cooler19860212@yahoo.com>
2024-06-24 17:19:34 +0300

    Go/Learn: add generics

--- Learning/main.go
+++ Learning/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
 
 func Add(a int, b int) int {
 	return a + b
 }
+
+func AddType[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
 func main() {
-
 	fmt.Println(Add(1, 2))
-
+	fmt.Println(AddType(1, 2.40))
+	fmt.Println(AddType("apple ", "banana"))
 }
