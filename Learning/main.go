68e70e8cb03882a5cf1c6f5fb0369a8db45a9651
coolerSAI <cooler19860212@yahoo.com>
2024-06-24 16:03:45 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
 
-	go func() {
-		time.Sleep(1 * time.Second)
-		ch1 <- 1
-	}()
-
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch2 <- 2
-	}()
-
-	select {
-	case <-ch1:
-		fmt.Println("Add Data from chanel 1")
-	case <-ch2:
-		fmt.Println("Add Data from chanel 2")
-	default:
-		fmt.Println("No data added")
-	}
 }
