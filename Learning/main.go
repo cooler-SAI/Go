a0ec243d396ffe2c0f7a81fc64279c41ba737151
cooler-SAI <cooler19860212@yahoo.com>
2024-06-25 14:37:14 +0300

    Go/Learn: added date issue format

--- Learning/main.go
+++ Learning/main.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	toChange := "Hello here!"
+	tz, err := time.LoadLocation("Europe/London")
+	if err != nil {
+		panic(err)
+	}
 
-	var pointer = &toChange
-	fmt.Println(*pointer)
+	dates := []time.Time{
+		time.Date(2024, 1, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 2, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 3, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 4, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 5, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 6, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 7, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 8, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 9, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 10, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 11, 1, 0, 0, 0, 0, tz),
+		time.Date(2024, 12, 1, 0, 0, 0, 0, tz),
+	}
+
+	for _, date := range dates {
+		fmt.Println(date.Format(time.RFC3339))
+	}
 }
