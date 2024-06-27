b55aec8654ec20166c9e0fd6c9204e4d4f49ca4f
coolerSAI <cooler19860212@yahoo.com>
2024-06-27 19:58:20 +0300

    Go/Learn: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"main.go/hello"
-)
-
 func main() {
-
-	fmt.Println(hello.Hello())
-
-	fmt.Println("Errors...")
-
-	message := hello.HelloNew("Gladys")
-	fmt.Println(message)
-
-	function := hello.Result(10, 20)
-	fmt.Println(function)
-
-	function2 := hello.HelloDificult("Ander")
-	function3 := hello.HelloDificult("")
-	fmt.Println(function2, function3)
 
 }
