1b99044743f959561a7ab1923a7018cf0cd70534
coolerSAI <cooler19860212@yahoo.com>
2024-06-28 09:10:39 +0300

    Go/Learning: set up simple json

--- Learning/learning.go
+++ Learning/learning.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
 func main() {
+	jsonString := `{"name": "John", "age": 30, "email": "john@example.com"}`
+	var person Person
+
+	err := json.Unmarshal([]byte(jsonString), &person)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	fmt.Println("Name:", person.Name)
+	fmt.Println("Age:", person.Age)
+	fmt.Println("Email:", person.Email)
 }
