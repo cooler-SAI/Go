d4d8862e7d5784b1aef0ba86692a303c3a1b910e
cooler-SAI <cooler19860212@yahoo.com>
2024-06-29 14:38:05 +0300

    Go/Learning: added new JSON files

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Cafe struct {
+	Name   string `json:"name"`
+	Place  string `json:"place"`
+	Number int    `json:"number"`
+}
+
 func main() {
+
+	jsonString := `{"name":"John","place":"Doe","number":42}`
+	var cafe Cafe
+	err := json.Unmarshal([]byte(jsonString), &cafe)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("New cafe:", cafe)
 
 }
