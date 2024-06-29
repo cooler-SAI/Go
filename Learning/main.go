42c0aa7c0d4dd77e4213e51b495b9f1ee354d951
coolerSAI <cooler19860212@yahoo.com>
2024-06-29 22:00:37 +0300

    Go/Learning: added new JSON, Info

--- Learning/main.go
+++ Learning/main.go
@@ -9,6 +9,20 @@ type Bird struct {
 	Species string `json:"Knurl"`
 }
 
+type ManInfo struct {
+	Name     string   `json:"name"`
+	Age      int      `json:"age"`
+	Email    string   `json:"email"`
+	IsActive bool     `json:"isActive"`
+	Roles    []string `json:"roles"`
+	Address  struct {
+		Street     string `json:"street"`
+		City       string `json:"city"`
+		State      string `json:"state"`
+		PostalCode string `json:"postalCode"`
+	} `json:"address"`
+}
+
 func main() {
 	pigeon := &Bird{
 		Species: "Pigeon",
