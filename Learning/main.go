fa6f253d9827682e0c43e799146ea43ff8f80db9
cooler-SAI <cooler19860212@yahoo.com>
2024-06-29 15:02:11 +0300

    Go/Learning: added JSON streaming

--- Learning/main.go
+++ Learning/main.go
@@ -2,36 +2,26 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
+	"os"
 )
 
-type Cafe struct {
-	Name   string `json:"name"`
-	Place  string `json:"place"`
-	Number int    `json:"number"`
-}
-
 func main() {
-
-	jsonString := `{"name":"John","place":"Doe","number":42}`
-	var cafe Cafe
-	err := json.Unmarshal([]byte(jsonString), &cafe)
-	if err != nil {
-		fmt.Println(err)
-		return
+	dec := json.NewDecoder(os.Stdin)
+	enc := json.NewEncoder(os.Stdout)
+	for {
+		var v map[string]interface{}
+		if err := dec.Decode(&v); err != nil {
+			log.Println(err)
+			return
+		}
+		for k := range v {
+			if k != "Name" {
+				delete(v, k)
+			}
+		}
+		if err := enc.Encode(&v); err != nil {
+			log.Println(err)
+		}
 	}
-	fmt.Println("New cafe:", cafe)
-
-	var littleCafe = Cafe{
-		Name:   "Rooms",
-		Place:  "Oldenburg",
-		Number: 25,
-	}
-	jSonBytes, err := json.Marshal(littleCafe)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(jSonBytes))
-
 }
