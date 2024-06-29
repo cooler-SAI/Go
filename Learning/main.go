f8f8a8afb22c41bc03c9b02c62af94856c81f388
coolerSAI <cooler19860212@yahoo.com>
2024-06-29 22:15:45 +0300

    Go/Learning: more JSON tests

--- Learning/main.go
+++ Learning/main.go
@@ -23,18 +23,46 @@ type ManInfo struct {
 	} `json:"address"`
 }
 
+type Country struct {
+	Country         string `json:"country"`
+	DisplayName     string `json:"display_name"`
+	Email           string `json:"email"`
+	ExplicitContent struct {
+		FilterEnabled bool `json:"filter_enabled"`
+		FilterLocked  bool `json:"filter_locked"`
+	} `json:"explicit_content"`
+	ExternalUrls struct {
+		Spotify string `json:"spotify"`
+	} `json:"external_urls"`
+	Followers struct {
+		Href  string `json:"href"`
+		Total int    `json:"total"`
+	} `json:"followers"`
+	Href   string `json:"href"`
+	ID     string `json:"id"`
+	Images []struct {
+		URL    string `json:"url"`
+		Height int    `json:"height"`
+		Width  int    `json:"width"`
+	} `json:"images"`
+	Product string `json:"product"`
+	Type    string `json:"type"`
+	URI     string `json:"uri"`
+}
+
 func main() {
+	// Marshal Bird struct to JSON
 	pigeon := &Bird{
 		Species: "Pigeon",
 	}
 
 	data, err := json.Marshal(pigeon)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error marshalling Bird struct:", err)
 		return
 	}
 
-	fmt.Println(string(data))
+	fmt.Println("Marshalled Bird JSON:", string(data))
 
 	jsonStr := `{
 		"name": "John Doe",
@@ -49,6 +77,7 @@ func main() {
 			"postalCode": "12345"
 		}
 	}`
+
 	var man ManInfo
 	err = json.Unmarshal([]byte(jsonStr), &man)
 	if err != nil {
@@ -57,4 +86,53 @@ func main() {
 	}
 
 	fmt.Printf("Unmarshalled ManInfo struct: %+v\n", man)
+
+	country := &Country{
+		Country:     "UK",
+		DisplayName: "United Kingdom",
+		Email:       "info@uk.gov",
+		ExplicitContent: struct {
+			FilterEnabled bool `json:"filter_enabled"`
+			FilterLocked  bool `json:"filter_locked"`
+		}{
+			FilterEnabled: true,
+			FilterLocked:  false,
+		},
+		ExternalUrls: struct {
+			Spotify string `json:"spotify"`
+		}{
+			Spotify: "https://spotify.com/uk",
+		},
+		Followers: struct {
+			Href  string `json:"href"`
+			Total int    `json:"total"`
+		}{
+			Href:  "",
+			Total: 1000000,
+		},
+		Href: "https://example.com/uk",
+		ID:   "uk",
+		Images: []struct {
+			URL    string `json:"url"`
+			Height int    `json:"height"`
+			Width  int    `json:"width"`
+		}{
+			{
+				URL:    "https://example.com/image1.jpg",
+				Height: 100,
+				Width:  100,
+			},
+		},
+		Product: "premium",
+		Type:    "country",
+		URI:     "spotify:country:uk",
+	}
+
+	countryData, err := json.Marshal(country)
+	if err != nil {
+		fmt.Println("Error marshalling Country struct:", err)
+		return
+	}
+
+	fmt.Println("Marshalled Country JSON:", string(countryData))
 }
