dbcaecf6c62601409b29ef70eecfaa35a06d693e
cooler-SAI <cooler19860212@yahoo.com>
2024-06-30 10:47:19 +0300

    Go/Learning: Go generics add

--- Learning/go.mod
+++ Learning/go.mod
@@ -1,3 +1,5 @@
 module Learning
 
 go 1.22
+
+require golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
+
+func Max[T constraints.Ordered](a T, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
 func main() {
+
+	fmt.Println(Max(1, 2))
 
 }
