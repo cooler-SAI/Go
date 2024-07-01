9e6ae1dc50a97e03180d085a8f92cd608bb59718
cooler-SAI <cooler19860212@yahoo.com>
2024-07-01 23:39:09 +0300

    Go/Learning: simple test, slices, again... :\

--- Learning/main.go
+++ Learning/main.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Println(mySlice)
 	fmt.Println(len(mySlice))
 	fmt.Println(cap(mySlice))
+	mySlice2 := make([]int, 5, 10)
+	fmt.Printf("myslice1 = %v\n", mySlice2)
+	fmt.Printf("length = %d\n", len(mySlice2))
+	fmt.Printf("capacity = %d\n", cap(mySlice2))
 }
