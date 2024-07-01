061ec60d8e8c6fd826f8fcb6945e1f7dd17dbdba
cooler-SAI <cooler19860212@yahoo.com>
2024-07-01 23:32:15 +0300

    Go/Learning: simple test, array :\

--- Learning/main.go
+++ Learning/main.go
@@ -5,17 +5,8 @@ import (
 )
 
 func main() {
-	var i = 15
-
-	fmt.Printf("%b\n", i)
-	fmt.Printf("%d\n", i)
-	fmt.Printf("%+d\n", i)
-	fmt.Printf("%o\n", i)
-	fmt.Printf("%O\n", i)
-	fmt.Printf("%x\n", i)
-	fmt.Printf("%X\n", i)
-	fmt.Printf("%#x\n", i)
-	fmt.Printf("%4d\n", i)
-	fmt.Printf("%-4d\n", i)
-	fmt.Printf("%04d\n", i)
+	var arr = [3]int{1, 2, 3}
+	fmt.Println(arr)
+	arr2 := [...]int{1, 2, 3}
+	fmt.Println(arr2)
 }
