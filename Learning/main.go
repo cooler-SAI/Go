8e2ae54be901427ae952d979320b883e4fd9d838
cooler-SAI <cooler19860212@yahoo.com>
2024-07-01 21:18:14 +0300

    Go/Learning: simple test, base added, again, here :\

--- Learning/main.go
+++ Learning/main.go
@@ -7,10 +7,14 @@ const A = 25
 
 func main() {
 	fmt.Printf("PI: %f\n", PI)
-	fmt.Printf("A: %f\n", A)
+	fmt.Printf("A: %v\n", A)
 	var i, j = "Hello ", "World"
 
 	fmt.Print(i)
 	fmt.Print(j)
+
+	var k, l = "Hello", "World"
+
+	fmt.Println("\n", k, l)
 
 }
