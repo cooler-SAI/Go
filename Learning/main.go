fca68514a932e72febbb07aa1a9af074da7863e5
cooler-SAI <cooler19860212@yahoo.com>
2024-07-01 23:44:59 +0300

    Go/Learning: simple test, slices, append :\

--- Learning/main.go
+++ Learning/main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	mySlice := []int{1, 2, 3}
-	fmt.Println(mySlice)
-	fmt.Println(len(mySlice))
-	fmt.Println(cap(mySlice))
-	mySlice2 := make([]int, 5, 10)
-	fmt.Printf("myslice1 = %v\n", mySlice2)
-	fmt.Printf("length = %d\n", len(mySlice2))
-	fmt.Printf("capacity = %d\n", cap(mySlice2))
-	mySlice2[0] = 100
-	fmt.Println(mySlice2)
-	fmt.Printf("length = %d\n", len(mySlice2))
+	mySlice2 := []int{4, 5, 6}
+
+	mySlice3 := append(mySlice, mySlice2...)
+	fmt.Println(mySlice3)
 }
