a7066120f24ca5eb4ea9e91e26978079d0525515
cooler-SAI <cooler19860212@yahoo.com>
2024-07-01 21:00:41 +0300

    Go/Learning: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"golang.org/x/exp/constraints"
-)
-
-func Max[T constraints.Ordered](a T, b T) T {
-	if a > b {
-		return a
-	}
-	return b
-}
 func main() {
-
-	fmt.Println(Max(1, 2))
 
 }
