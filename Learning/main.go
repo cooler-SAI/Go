1abf437278b12919c7930419a9d516bc316ea68a
cooler-SAI <cooler19860212@yahoo.com>
2024-07-03 00:09:15 +0300

    Go/Learning: simple test, funcs, more :\

--- Learning/main.go
+++ Learning/main.go
@@ -8,9 +8,15 @@ func familyName(fname string) {
 	fmt.Println("familyName:", fname)
 }
 
+func familyFullInfo(fname string, fage int) {
+	fmt.Println("familyFullInfo:", fname, fage)
+}
+
 func main() {
 
 	familyName("Ander")
 	familyName("Bob")
+	familyFullInfo("Ander", 38)
+	familyFullInfo("Bob", 20)
 
 }
