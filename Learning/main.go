b6fe88096de2b9c37d881f82d58bfa2c78e8e347
cooler-SAI <cooler19860212@yahoo.com>
2024-07-02 23:51:41 +0300

    Go/Learning: simple test, added if :\

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var x, y = 500, 300
+	fmt.Println(x, y)
+	if true {
+		fmt.Printf("x is equal to y, %v\n", x)
+
+	} else {
+		fmt.Println("x is greater than y")
+	}
 
 }
