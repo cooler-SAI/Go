06e85d8458ad5cf1db4a5d1cb802e4d4bba04ff1
cooler-SAI <cooler19860212@yahoo.com>
2024-07-02 23:57:05 +0300

    Go/Learning: simple test, if inside if :\

--- Learning/main.go
+++ Learning/main.go
@@ -28,5 +28,14 @@ func main() {
 	} else {
 		fmt.Println("Good evening.")
 	}
+	num := 20
+	if num >= 10 {
+		fmt.Println("Num is more than 10.")
+		if num > 15 {
+			fmt.Println("Num is also more than 15.")
+		}
+	} else {
+		fmt.Println("Num is less than 10.")
+	}
 
 }
