e178810319e942d1a31fa64a629939a0d327dc94
cooler-SAI <cooler19860212@yahoo.com>
2024-07-03 00:00:57 +0300

    Go/Learning: simple test, for using :\

--- Learning/main.go
+++ Learning/main.go
@@ -3,17 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	day := 4
 
-	switch day {
-	case 1, 3, 5:
-		fmt.Println("Odd weekday")
-	case 2, 4:
-		fmt.Println("Even weekday")
-	case 6, 7:
-		fmt.Println("Weekend")
-	default:
-		fmt.Println("Invalid day of day number")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+
 	}
 
 }
