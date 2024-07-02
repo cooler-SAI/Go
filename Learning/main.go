a6e948e31f04225d8baa63011d90e3050ddbca8d
cooler-SAI <cooler19860212@yahoo.com>
2024-07-03 00:28:13 +0300

    Go/Learning: simple test, maps...... :\

--- Learning/main.go
+++ Learning/main.go
@@ -4,25 +4,10 @@ import (
 	"fmt"
 )
 
-func familyName(fname string) {
-	fmt.Println("familyName:", fname)
-}
-
-func familyFullInfo(fname string, fage int) {
-	fmt.Println("familyFullInfo:", fname, fage)
-}
-
-func resultTest(x, y int) (int, error) {
-	z := x + y
-	return fmt.Println("result will be:", z)
-}
-
 func main() {
+	var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
 
-	familyName("Ander")
-	familyName("Bob")
-	familyFullInfo("Ander", 38)
-	familyFullInfo("Bob", 20)
-	resultTest(20, 40)
-
+	fmt.Printf("a\t%v\n", a)
+	fmt.Printf("b\t%v\n", b)
 }
