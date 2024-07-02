f14efce8db13a3700bd5286110ff403509e988af
cooler-SAI <cooler19860212@yahoo.com>
2024-07-02 23:58:39 +0300

    Go/Learning: simple test, switch tested :\

--- Learning/main.go
+++ Learning/main.go
@@ -3,39 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	day := 4
 
-	var x, y = 500, 300
-	fmt.Println(x, y)
-	if true {
-		fmt.Printf("x is equal to y, %v\n", x)
-
-	} else {
-		fmt.Println("x is greater than y")
-	}
-
-	time := 20
-	if false {
-		fmt.Println("Good day.", time)
-	} else {
-		fmt.Println("Good evening.", time)
-	}
-
-	time2 := 22
-	if false {
-		fmt.Println("Good morning.")
-	} else if time2 < 20 {
-		fmt.Println("Good day.")
-	} else {
-		fmt.Println("Good evening.")
-	}
-	num := 20
-	if num >= 10 {
-		fmt.Println("Num is more than 10.")
-		if num > 15 {
-			fmt.Println("Num is also more than 15.")
-		}
-	} else {
-		fmt.Println("Num is less than 10.")
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
 	}
 
 }
