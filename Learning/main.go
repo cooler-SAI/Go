8142830d325d692f3d34491fb99ec1694063b31c
cooler-SAI <cooler19860212@yahoo.com>
2024-07-03 00:35:52 +0300

    Go/Learning: simple test, maps. new one :\

--- Learning/main.go
+++ Learning/main.go
@@ -5,17 +5,10 @@ import (
 )
 
 func main() {
-	var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
-	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
 
-	fmt.Printf("a\t%v\n", a)
-	fmt.Printf("b\t%v\n", b)
-
-	c := make(map[string]int)
-	c["Oslo"] = 1
-	c["Bergen"] = 2
-	c["Trondheim"] = 3
-	fmt.Printf("c\t%v\n", c)
-	fmt.Printf("b\t%v\n", b)
-
+	fmt.Printf(a["brand"])
 }
