445ee8f91acd84d3c259962b2c755014d03275e2
cooler-SAI <cooler19860212@yahoo.com>
2024-07-04 22:31:58 +0300

    Go/Learning: simple testing, new one

--- Learning/testing/testing.go
+++ Learning/testing/testing.go
@@ -3,3 +3,7 @@ package testing
 func SimpleTest() string {
 	return "good"
 }
+
+func SimpleTest2() int {
+	return 2
+}
--- Learning/testing/testing_test.go
+++ Learning/testing/testing_test.go
@@ -4,9 +4,16 @@ import (
 	"testing"
 )
 
-func TestSimpleTestFunc(t *testing.T) {
+/*func TestSimpleTestFunc(t *testing.T) {
 	expectedSimpleTestResult := "good"
 	if actualSimpleTestResult := SimpleTest(); actualSimpleTestResult != expectedSimpleTestResult {
 		t.Errorf("expected %s; got: %s", expectedSimpleTestResult, actualSimpleTestResult)
+	}
+}*/
+
+func TestSimpleTest2Func(t *testing.T) {
+	expectedSimpleTestResult := 2
+	if actualSimpleTestResult := SimpleTest2(); actualSimpleTestResult != expectedSimpleTestResult {
+		t.Errorf("expected %d; got: %d", expectedSimpleTestResult, actualSimpleTestResult)
 	}
 }
