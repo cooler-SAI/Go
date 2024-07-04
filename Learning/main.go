39e2c0ef937557fbd25e6d7b8cd49b3c31d08a0f
cooler-SAI <cooler19860212@yahoo.com>
2024-07-04 22:05:36 +0300

    Go/Learning: simple test, maps  added also, again :\

--- Learning/main.go
+++ Learning/main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	a := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
 	fmt.Println(a)
+
+	b := make(map[string]int)
+	b["a"] = 1
+	b["b"] = 2
+	b["c"] = 3
+	fmt.Println(b)
 }
