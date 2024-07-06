0ce92e980eb407de0e0f8671403175d7e80d0c0a
cooler-SAI <cooler19860212@yahoo.com>
2024-07-06 12:58:53 +0300

    Go/Learning: more tests added, iota :\

--- Learning/main.go
+++ Learning/main.go
@@ -2,24 +2,13 @@ package main
 
 import "fmt"
 
-type Weekday int
-
 const (
-	Monday Weekday = iota + 1
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-	Sunday
+	a = iota // 0
+	b        // 1
+	c        // 2
 )
 
-func NextDay(day Weekday) Weekday {
-	return (day % 7) + 1
-}
-
 func main() {
-	var today Weekday = Sunday
-	tomorrow := NextDay(today)
-	fmt.Println("today =", today, "tomorrow =", tomorrow)
+	fmt.Println(a, b, c)
+
 }
