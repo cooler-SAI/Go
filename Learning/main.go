e6c4ccc3c7b1a00743fc295bca109d9ec7e5c34c
cooler-SAI <cooler19860212@yahoo.com>
2024-07-06 12:31:24 +0300

    Go/Learning: added type print

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	ver := "v0.0.1"
+	id := 0
+	pi := 3.1415
+	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+
+	fmt.Printf("ver = %v (type %T)\n", ver, ver)
+	fmt.Printf("id = %v (type %T)\n", id, id)
+	fmt.Printf("pi = %v (type %T)\n", pi, pi)
 
 }
