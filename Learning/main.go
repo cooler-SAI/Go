2069460f428324fe56a6910e01f315dfcd938585
cooler-SAI <cooler19860212@yahoo.com>
2024-07-06 11:44:31 +0300

    Go/Learning: clean up

--- Learning/main.go
+++ Learning/main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
-	fmt.Println(a)
 
-	b := make(map[string]int)
-	b["a"] = 1
-	b["b"] = 2
-	b["c"] = 3
-	fmt.Println(b)
-	b["d"] = 40
-	fmt.Println(b)
-	fmt.Println(b["d"])
 }
