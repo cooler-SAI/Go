30f6b938a49cb89346e31e843f6e64b1b54104d1
cooler-SAI <cooler19860212@yahoo.com>
2024-07-08 14:51:54 +0300

    Go/Learning: set up simple temp :/

--- Main/main.go
+++ Main/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	name        string
+	age         int
+	lastVisited time.Time
+}
+
+func UpdatePersonWithLastVisited(p *Person) {
+	p.lastVisited = time.Now()
+}
 
 func main() {
 	var a int
@@ -11,4 +24,12 @@ func main() {
 	p2 := &b
 	fmt.Println(*p2, p2, b)
 
+	person := Person{
+		name: "Alex",
+		age:  25,
+	}
+	UpdatePersonWithLastVisited(&person)
+
+	fmt.Printf("Name: %s, Age: %d, LastVisited: %v\n", person.name,
+		person.age, person.lastVisited)
 }
