3c15797eb9d3f370d3d19cbf1db50ec93179b502
cooler-SAI <cooler19860212@yahoo.com>
2024-07-08 14:56:22 +0300

    Go/Learning: set up simple test benchmark :/

--- Main/main.go
+++ Main/main.go
@@ -5,31 +5,12 @@ import (
 	"time"
 )
 
-type Person struct {
-	name        string
-	age         int
-	lastVisited time.Time
-}
-
-func UpdatePersonWithLastVisited(p *Person) {
-	p.lastVisited = time.Now()
-}
-
 func main() {
-	var a int
-	p := &a
-	fmt.Println(*p, p, a)
-
-	b := "Hello Here!"
-	p2 := &b
-	fmt.Println(*p2, p2, b)
-
-	person := Person{
-		name: "Alex",
-		age:  25,
+	start := time.Now()
+	sum := 0
+	for i := 0; i < 100000000; i++ {
+		sum += i
 	}
-	UpdatePersonWithLastVisited(&person)
-
-	fmt.Printf("Name: %s, Age: %d, LastVisited: %v\n", person.name,
-		person.age, person.lastVisited)
+	fmt.Println("Sum:", sum)
+	fmt.Println("Time elapsed:", time.Since(start))
 }
