624591d415b06b379fc160a34a208fcc9a5d167f
cooler-SAI <cooler19860212@yahoo.com>
2024-07-08 17:08:37 +0300

    Go/Learning: clean up :/

--- Main/main.go
+++ Main/main.go
@@ -1,21 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	a := 1
-	p := &a
-	b := &p
-
-	*p = 3
-	**b = 5
-
-	a += 4 + *p + **b
-
-	fmt.Printf("%d", *p)
-
-	myMap := map[int]string{1: "a", 2: "b", 3: "c"}
-	fmt.Println(myMap)
 }
