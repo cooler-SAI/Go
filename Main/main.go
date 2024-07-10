05381caa7a5a52c5d7270e60b6a118838e43e997
cooler-SAI <cooler19860212@yahoo.com>
2024-07-10 23:08:37 +0300

    Go/Learning: added simple func again... :/

--- Main/main.go
+++ Main/main.go
@@ -5,7 +5,12 @@ import "fmt"
 func Cube(x int) int {
 	return x * x * x
 }
+
+func Mul(x int, y int) int {
+	return x * y * y
+}
 func main() {
 	check := Cube(3)
 	fmt.Println(check)
+	fmt.Println(Mul(3, 30))
 }
