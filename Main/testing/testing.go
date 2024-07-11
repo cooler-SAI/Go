3b55543d5a07a401a3605d38773f8ebf9febe433
cooler-SAI <cooler19860212@yahoo.com>
2024-07-11 21:44:53 +0300

    Go/Learning: added tests :/

--- Main/testing/testing.go
+++ Main/testing/testing.go
@@ -1,9 +1,22 @@
 package testing
 
+import "errors"
+
 func SimpleTest() string {
 	return "good"
 }
 
 func SimpleTest2() int {
 	return 2
+}
+
+func Add(a, b int) (int, error) {
+	if a == 0 || b == 0 {
+		return 0, errors.New("arg is zero")
+	}
+
+	if a < 0 || b < 0 {
+		return 0, errors.New("arg is negative")
+	}
+	return a + b, nil
 }
--- Main/testing/testing_test.go
+++ Main/testing/testing_test.go
@@ -17,3 +17,28 @@ func TestSimpleTest2Func(t *testing.T) {
 		t.Errorf("expected %d; got: %d", expectedSimpleTestResult, actualSimpleTestResult)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	sum, err := Add(1, 2)
+	if err != nil {
+		t.Error("unexpected error")
+	}
+	if sum != 3 {
+		t.Errorf("sum expected to be 3; got %d", sum)
+	}
+}
+
+func TestAddZero(t *testing.T) {
+	_, err := Add(0, 2)
+	if err == nil {
+		t.Error("first arg is zero  - expected error not be nil")
+	}
+	_, err = Add(1, 0)
+	if err == nil {
+		t.Error("second arg is zero  - expected error not be nil")
+	}
+	_, err = Add(0, 0)
+	if err == nil {
+		t.Error("all arg negative - expected error not be nil")
+	}
+}
