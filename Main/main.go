61a9355a563075502a6a3bba145846d8434eb76c
cooler-SAI <cooler19860212@yahoo.com>
2024-07-11 12:33:41 +0300

    Go/Learning: added structs :/

--- Main/main.go
+++ Main/main.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type MyInt int
 
+type OuterStruct struct {
+	EmbeddedType
+	A int
+	B int
+}
+
+type EmbeddedType struct {
+	A string
+	B string
+}
+
+func (o OuterStruct) Print() {
+	fmt.Println("A=", o.A, "B=", o.B)
+}
+
 func (my MyInt) Set(v int) {
 	my = MyInt(v)
 }
@@ -12,4 +27,7 @@ func main() {
 	var myInt MyInt
 	myInt.Set(5)
 	fmt.Println(myInt)
+
+	var check OuterStruct
+	check.Print()
 }
