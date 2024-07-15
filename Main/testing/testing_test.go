5809e2fb31717d27696b113ad34bfb405def3a0c
cooler-SAI <cooler19860212@yahoo.com>
2024-07-15 22:18:28 +0300

    Go/Learning: added new tests, more :/

--- Main/testing/testing_test.go
+++ Main/testing/testing_test.go
@@ -104,3 +104,33 @@ func TestEstimateValue(t *testing.T) {
 		assert.Equal(t, "big", EstimateValue(100))
 	})
 }
+
+func TestEstimateValueTableDriven(t *testing.T) {
+	testCases := []struct {
+		Name          string
+		InputValue    int
+		ExpectedValue string
+	}{
+		{
+			Name:          "Small",
+			InputValue:    9,
+			ExpectedValue: "small",
+		},
+		{
+			Name:          "Medium",
+			InputValue:    99,
+			ExpectedValue: "medium",
+		},
+		{
+			Name:          "Big",
+			InputValue:    100,
+			ExpectedValue: "big",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			assert.EqualValues(t, tc.ExpectedValue, EstimateValue(tc.InputValue))
+		})
+	}
+}
