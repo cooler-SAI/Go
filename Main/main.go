7c6702ef7f613e79990899565ce7bb57f28b3f83
cooler-SAI <cooler19860212@yahoo.com>
2024-07-15 22:05:47 +0300

    Go/Learning: added base tests :/

--- Main/main.go
+++ Main/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ExampleTestSuite struct {
+	suite.Suite
+	VariableThatShouldStartAtFive int
+}
+
+func (suite *ExampleTestSuite) SetupTest() {
+	suite.VariableThatShouldStartAtFive = 5
+}
+
+func (suite *ExampleTestSuite) TestExample() {
+	suite.Equal(5, suite.VariableThatShouldStartAtFive)
+}
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, new(ExampleTestSuite))
+}
+
 func main() {
 
 }
