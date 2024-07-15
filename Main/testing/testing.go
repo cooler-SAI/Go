8f33203715d3b0725126977c5e0172ce311e2502
cooler-SAI <cooler19860212@yahoo.com>
2024-07-15 22:17:37 +0300

    Go/Learning: added base tests, more :/

--- Main/testing/testing.go
+++ Main/testing/testing.go
@@ -43,3 +43,16 @@ func Division(a, b int) (int, error) {
 	}
 	return a + b, nil
 }
+
+func EstimateValue(value int) string {
+	switch {
+	case value < 10:
+		return "small"
+
+	case value < 100:
+		return "medium"
+
+	default:
+		return "big"
+	}
+}
--- Main/testing/testing_test.go
+++ Main/testing/testing_test.go
@@ -90,3 +90,17 @@ func TestDivision(t *testing.T) {
 		fmt.Println(err)
 	})
 }
+
+func TestEstimateValue(t *testing.T) {
+	t.Run("Small", func(t *testing.T) {
+		assert.Equal(t, "small", EstimateValue(9))
+	})
+
+	t.Run("Medium", func(t *testing.T) {
+		assert.Equal(t, "medium", EstimateValue(99))
+	})
+
+	t.Run("Big", func(t *testing.T) {
+		assert.Equal(t, "big", EstimateValue(100))
+	})
+}
