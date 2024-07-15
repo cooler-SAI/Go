27167f3ed648e38689f3c4f461308bb8cb1705cb
cooler-SAI <cooler19860212@yahoo.com>
2024-07-15 12:13:12 +0300

    Go/Learning: added more unit tests, again... :/

--- Main/simple/simple_test.go
+++ Main/simple/simple_test.go
@@ -1,1 +1,11 @@
 package simple
+
+import "testing"
+
+func TestWordTest(t *testing.T) {
+	expected := WordTest()
+	if WordTest() != expected {
+		t.Error("Expected", expected, "got", WordTest())
+	}
+
+}
