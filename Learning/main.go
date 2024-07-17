c84b8335ba4b387b14ab7a57bcd2b021a1ba71d2
cooler-SAI <cooler19860212@yahoo.com>
2024-07-17 12:06:11 +0300

    Go/Learning: added Cobra CLI, updates

--- Learning/main.go
+++ Learning/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
 func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "learning",
+		Short: "learning is an example of a cobra command short",
+		Long:  `learning is an example of a cobra command long`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, Cobra!")
+		},
+	}
+	var echoCmd = &cobra.Command{
+		Use:   "echo [string to echo]",
+		Short: "Echo a string",
+		Long:  "Echo is a subcommand that prints the input string to the terminal.",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(args[0])
+		},
+	}
+	rootCmd.AddCommand(echoCmd)
+	rootCmd.Execute()
 
 }
