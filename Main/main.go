113d43f44ea65fa89880f5234c7460f42d9b7820
cooler-SAI <cooler19860212@yahoo.com>
2024-07-18 14:39:39 +0300

    Go/Learning: clean up

--- Main/main.go
+++ Main/main.go
@@ -1,28 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
-type User struct {
-	ID   int
-	Name string
-}
-
-type RealUserService struct{}
-
-func (s *RealUserService) GetUser(id int) (*User, error) {
-
-	return &User{ID: id, Name: "John Doe"}, nil
-}
-
 func main() {
-	service := &RealUserService{}
-	user, err := service.GetUser(1)
-	if err != nil {
-		fmt.Println("Error fetching user:", err)
-		return
-	}
-
-	fmt.Println("User fetched:", *user)
 }
