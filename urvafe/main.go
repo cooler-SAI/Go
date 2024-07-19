63da306b3f3cfc808dc42a8881d4a8033e9755a4
cooler-SAI <cooler19860212@yahoo.com>
2024-07-19 20:49:54 +0300

    Go/Learning: added urvafe base command

--- urvafe/main.go
+++ urvafe/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"os"
+)
 
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "hello",
+				Aliases: []string{"h"},
+				Usage:   "prints Hello urfave",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Hello urfave")
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
