a530685eb738abcb5b23004faa424dc10d741015
cooler-SAI <cooler19860212@yahoo.com>
2024-07-19 21:06:32 +0300

    Go/Learning: added urvafe new one

--- urvafe/main.go
+++ urvafe/main.go
@@ -17,6 +17,14 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:  "goodbye",
+				Usage: "prints Goodbye urfave",
+				Action: func(c *cli.Context) error {
+					fmt.Println("Goodbye urfave")
+					return nil
+				},
+			},
 		},
 	}
 
