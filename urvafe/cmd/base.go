67188f8b99602324fa95ced658ae4e26ecec7887
cooler-SAI <cooler19860212@yahoo.com>
2024-07-21 19:49:53 +0300

    Go/Learning: added base urvafe flags

--- urvafe/cmd/base.go
+++ urvafe/cmd/base.go
@@ -13,24 +13,48 @@ func RunApp() {
 				{
 					Name:  "hello",
 					Usage: "prints Hello urfave",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:  "name",
+							Value: "urfave",
+							Usage: "name to greet",
+						},
+					},
 					Action: func(c *cli.Context) error {
-						fmt.Println("Hello urfave")
+						name := c.String("name")
+						fmt.Printf("Hello %s\n", name)
 						return nil
 					},
 				},
 				{
 					Name:  "goodbye",
 					Usage: "prints Goodbye urfave",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:  "name",
+							Value: "urfave",
+							Usage: "name to bid goodbye",
+						},
+					},
 					Action: func(c *cli.Context) error {
-						fmt.Println("Goodbye urfave")
+						name := c.String("name")
+						fmt.Printf("Goodbye %s\n", name)
 						return nil
 					},
 				},
 				{
 					Name:  "world",
 					Usage: "prints World urfave",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:  "name",
+							Value: "World",
+							Usage: "custom message",
+						},
+					},
 					Action: func(c *cli.Context) error {
-						fmt.Println("World")
+						name := c.String("name")
+						fmt.Printf("%s urfave\n", name)
 						return nil
 					},
 				},
