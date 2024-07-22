cfdd8a8933381851c2b8cc7bcf7aba93a74d5096
cooler-SAI <cooler19860212@yahoo.com>
2024-07-22 20:14:55 +0300

    Go/Learning: added simple GORM app

--- gorm/main.go
+++ gorm/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Id    uint
+	Name  string
+	Email string
+}
 
 func main() {
 	fmt.Println("GORM starting....")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err2 := db.AutoMigrate(&User{})
+	if err2 != nil {
+		return
+	}
+
+	db.Create(&User{Name: "Ander", Email: "ander@gmail.com"})
+
+	var user User
+	db.First(&user, 1)
+	db.First(&user, "name = ?", "Ander")
+
+	db.Model(&user).Update("name", "AnderMe")
+
+	db.Delete(&user, 1)
 }
