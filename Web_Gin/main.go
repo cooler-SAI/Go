0990aa3c9ef4c5ecac4ec0500548a4a8f20295ba
cooler-SAI <cooler19860212@yahoo.com>
2024-07-25 22:37:49 +0300

    Go/Learning: set up package Gin, more

--- Web_Gin/main.go
+++ Web_Gin/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("web_Gin running...")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello gin")
+	})
+
+	r.GET("/about", func(c *gin.Context) {
+		c.String(http.StatusOK, "About us page")
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
