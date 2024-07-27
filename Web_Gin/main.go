1751f943c2d632638b02093f3ee3c2e4a25a5428
cooler-SAI <cooler19860212@yahoo.com>
2024-07-27 23:48:45 +0300

    Go/Learning: Gin, clean up, misc updates

--- Web_Gin/main.go
+++ Web_Gin/main.go
@@ -9,7 +9,6 @@ import (
 
 func LoggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Log request details
 		fmt.Printf("Request: %s %s\n", c.Request.Method, c.Request.URL.Path)
 		c.Next()
 	}
@@ -19,6 +18,8 @@ func main() {
 	fmt.Println("web_Gin running...")
 
 	r1 := gin.Default()
+	r1.Use(LoggerMiddleware())
+
 	r1.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello gin, it's Server 1")
 	})
@@ -66,13 +67,16 @@ func main() {
 		})
 	}
 
+	r1.Static("/assets", "./assets")
+
 	r2 := gin.Default()
+	r2.Use(LoggerMiddleware())
+
 	r2.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello gin, it's Server 2")
 	})
 
 	r2.GET("/about", func(c *gin.Context) {
-		LoggerMiddleware()
 		c.String(http.StatusOK, "About us page Server 2")
 	})
 
