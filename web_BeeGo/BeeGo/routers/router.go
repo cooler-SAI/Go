c0d04b50ad8d928888979b5280bd3c2e1e981611
cooler-SAI <cooler19860212@yahoo.com>
2024-08-02 21:51:20 +0300

    Go/Learning: BeeGo - added user ID

--- web_BeeGo/BeeGo/routers/router.go
+++ web_BeeGo/BeeGo/routers/router.go
@@ -9,4 +9,5 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/hello", &controllers.HelloController{})
 	beego.Router("/about", &controllers.AboutController{})
+	beego.Router("/user/:id", &controllers.UserController{})
 }
---
+++ web_BeeGo/BeeGo/views/user.tpl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Page</title>
+</head>
+<body>
+    <h1>User Information</h1>
+    <p>{{.User}}</p>
+</body>
+</html>
