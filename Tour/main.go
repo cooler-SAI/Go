84bcec0a1e4a9444d9bc865413d0022908454e3f
cooler-SAI <cooler19860212@yahoo.com>
2024-08-11 16:43:28 +0300

    Go/Tour: set up fibonacci func

--- Tour/main.go
+++ Tour/main.go
@@ -1,20 +1,21 @@
 package main
 
-import "golang.org/x/tour/pic"
+import "fmt"
 
-func Pic(dx, dy int) [][]uint8 {
+func fibonacci() func() int {
+	a, b := 0, 1
 	
-	result := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		result[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			result[y][x] = uint8((x ^ y))
-		}
+	return func() int {
+		current := a
+		a, b = b, a+b
+		return current
 	}
-	return result
 }
 
 func main() {
-	pic.Show(Pic)
+	fib := fibonacci()
+	
+	for i := 0; i < 10; i++ {
+		fmt.Println(fib())
+	}
 }
-
