5a529ce6c2bf387188940ec3c2b3279e5566814c
cooler-SAI <cooler19860212@yahoo.com>
2024-08-11 15:29:50 +0300

    Go/Tour: update main file

--- Tour/main.go
+++ Tour/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-type testInt int
-
-func (i testInt) test() int {
-	return int(i)
-}
+import "fmt"
 
 func main() {
-	var minus = -10
-
-	var test = testInt(500 * minus)
-	fmt.Println(test.test())
+	fmt.Println("Hello, All Here")
 }
