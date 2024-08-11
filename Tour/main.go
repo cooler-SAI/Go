54a8f9fb08868f274500ea7946847e8ebf2d831a
cooler-SAI <cooler19860212@yahoo.com>
2024-08-11 15:58:18 +0300

    Go/Tour: update base func

--- Tour/main.go
+++ Tour/main.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"math"
-)
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
-	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
 }
 
