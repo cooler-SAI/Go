9acdb7928ef49f950aa952daead2bb35e37fc0b3
cooler-SAI <cooler19860212@yahoo.com>
2024-08-11 15:42:20 +0300

    Go/Tour: set up rand number

--- Tour/main.go
+++ Tour/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Welcome to the playground!")
-
-	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
+
