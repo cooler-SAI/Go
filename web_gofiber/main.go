0f632741555f4d0c650a17c8619ee49ff426166a
cooler-SAI <cooler19860212@yahoo.com>
2024-08-19 21:09:40 +0300

    Go/Learning: gofiber - added new route page

--- web_gofiber/main.go
+++ web_gofiber/main.go
@@ -10,6 +10,7 @@ func main() {
 	fiberApp.Get("/", handlers.HomePage)
 	fiberApp.Get("/about", handlers.AboutPage)
 	fiberApp.Get("/contact", handlers.ContactPage)
+	fiberApp.Get("/blog", handlers.BlogPage)
 
 	err := fiberApp.Listen(":3000")
 	if err != nil {
