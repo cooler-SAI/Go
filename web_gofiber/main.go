fa4b5242690bbdd8a83ee33e5aae592fc37906e4
cooler-SAI <cooler19860212@yahoo.com>
2024-08-19 20:47:02 +0300

    Go/Learning: gofiber - more func's added

--- web_gofiber/main.go
+++ web_gofiber/main.go
@@ -8,6 +8,18 @@ func main() {
 		return c.SendString("Hello, gofiber!!!")
 	})
 
+	fiberApp.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("This is the About page.")
+	})
+
+	fiberApp.Get("/contact", func(c *fiber.Ctx) error {
+		return c.SendString("This is the Contact page.")
+	})
+
+	fiberApp.Get("/blog/:id", func(c *fiber.Ctx) error {
+		return c.SendString("Blog ID: " + c.Params("id"))
+	})
+
 	err := fiberApp.Listen(":3000")
 	if err != nil {
 		return
