1aff575c8b131ddb6573c33da2b4ffd214a79cd3
cooler-SAI <cooler19860212@yahoo.com>
2024-08-20 21:06:45 +0300

    Go/Learning: Gorilla - added new router

--- web_Gorilla/main.go
+++ web_Gorilla/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Gorilla")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+
+	http.Handle("/", r)
+	fmt.Println("Server is listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func HomeHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to the Home Page!")
+	if err != nil {
+		return
+	}
+}
+
+func ProductsHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to the Products Page!")
+	if err != nil {
+		return
+	}
+}
+
+func ArticlesHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to the Articles Page!")
+	if err != nil {
+		return
+	}
 }
