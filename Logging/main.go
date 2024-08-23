90a07a7a178824a30b6f51542a6619f84de9a54f
cooler-SAI <cooler19860212@yahoo.com>
2024-08-23 22:56:41 +0300

    Go/Learning: added new log file

--- Logging/main.go
+++ Logging/main.go
@@ -5,13 +5,26 @@ import (
 	"os"
 )
 
-func main() {
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
 	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.SetOutput(file)
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
-	log.Println("Hello logs!")
+func main() {
+	InfoLogger.Println("Starting the application...")
+	InfoLogger.Println("Something noteworthy happened")
+	WarningLogger.Println("There is something you should know about")
+	ErrorLogger.Println("Something went wrong")
 }
