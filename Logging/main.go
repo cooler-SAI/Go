47248bcc1aeb99b7dfb9a43a419c0be4deb1fdd4
cooler-SAI <cooler19860212@yahoo.com>
2024-08-26 22:42:13 +0300

    Go/Learning: tests log/slog, own added

--- Logging/main.go
+++ Logging/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"log"
-	"log/slog"
 	"os"
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	logger.Info("hello, world", "user", os.Getenv("USER"))
 
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	log.Println("with micro")
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	log.Println("with file/line")
+
+	logger := log.New(os.Stdout, "my:", log.LstdFlags)
+	logger.Println("from logger")
 
 }
