fe8d806dde4487f5c54905e00bf1bba205e0da96
cooler-SAI <cooler19860212@yahoo.com>
2024-08-28 21:51:11 +0300

    Go/Learning: added zerolog JSON and os logs

--- Zerolog/main.go
+++ Zerolog/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 )
 
 func main() {
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Service RUN good!")
+
 	file, err := os.Create("Zerolog_logfile.log")
 	if err != nil {
 		log.Fatal().Err(err).Msg("Can't create Zerolog_logfile.log")
@@ -20,4 +26,6 @@ func main() {
 	log.Logger = log.Output(file)
 
 	log.Info().Msg("It's information file, wrote to JSON")
+
+	log.Info().Msg("Service RUN good in JSON!")
 }
