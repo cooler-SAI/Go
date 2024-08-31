5b9fb6b23a687ec736757cb73f1a1976d3caa12c
cooler-SAI <cooler19860212@yahoo.com>
2024-08-31 21:31:26 +0300

    Go/Learning: melody - added base logging

--- Melody/main.go
+++ Melody/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/olahol/melody"
 )
 
 func main() {
 	m := melody.New()
+
+	log.Print("Server started...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
@@ -16,6 +22,7 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		err := m.HandleRequest(w, r)
 		if err != nil {
+			log.Printf("Error handling WebSocket request: %v", err)
 			return
 		}
 	})
@@ -23,12 +30,21 @@ func main() {
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		err := m.Broadcast(msg)
 		if err != nil {
-			return
+			log.Printf("Error broadcasting message: %v", err)
 		}
 	})
 
-	err := http.ListenAndServe(":5000", nil)
-	if err != nil {
-		return
-	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := http.ListenAndServe(":5000", nil)
+		if err != nil {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	<-stop
+
+	log.Print("Server stopped.")
 }
