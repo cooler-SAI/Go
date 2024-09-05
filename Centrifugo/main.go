1324c50bacd2dd3b0a891ecd5dcca265bc6ab39e
cooler-SAI <cooler19860212@yahoo.com>
2024-09-05 20:45:33 +0300

    Go/Learning: Centrifugo - added base connection code

--- Centrifugo/main.go
+++ Centrifugo/main.go
@@ -10,28 +10,19 @@ import (
 func main() {
 	client := centrifuge.NewJsonClient("ws://localhost:8000/connection/websocket", centrifuge.Config{})
 
-	// Устанавливаем обработчик подключения
-	client.OnConnect(func(c *centrifuge.Client, e centrifuge.ConnectedEvent) {
+	client.OnConnect(func(c *centrifuge.Client, e centrifuge.ConnectEvent) {
 		log.Println("Connected to Centrifugo!")
 	})
-
-	// Устанавливаем обработчик отключения
-	client.OnDisconnect(func(c *centrifuge.Client, e centrifuge.DisconnectedEvent) {
+	client.OnDisconnect(func(c *centrifuge.Client, e centrifuge.DisconnectEvent) {
 		log.Println("Disconnected from Centrifugo!")
 	})
-
-	// Подключаемся к серверу
 	err := client.Connect()
 	if err != nil {
 		log.Fatalln("Connection error:", err)
 	}
-
-	// Публикуем сообщение в канал
 	err = client.Publish(context.Background(), "chat:room", []byte(`{"input": "Hello, World!"}`))
 	if err != nil {
 		log.Fatalln("Publish error:", err)
 	}
-
-	// Оставляем приложение запущенным
 	select {}
 }
