519f706017577e729583a768208440e0ce4e38c5
cooler-SAI <cooler19860212@yahoo.com>
2024-09-05 20:13:09 +0300

    Go/Learning: Centrifugo - added code to repo

--- Centrifugo/main.go
+++ Centrifugo/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/centrifugal/centrifuge-go"
+)
 
 func main() {
-	fmt.Println("Hello Centrifugo")
+	client := centrifuge.NewJsonClient("ws://localhost:8000/connection/websocket", centrifuge.Config{})
+
+	// Устанавливаем обработчик подключения
+	client.OnConnect(func(c *centrifuge.Client, e centrifuge.ConnectedEvent) {
+		log.Println("Connected to Centrifugo!")
+	})
+
+	// Устанавливаем обработчик отключения
+	client.OnDisconnect(func(c *centrifuge.Client, e centrifuge.DisconnectedEvent) {
+		log.Println("Disconnected from Centrifugo!")
+	})
+
+	// Подключаемся к серверу
+	err := client.Connect()
+	if err != nil {
+		log.Fatalln("Connection error:", err)
+	}
+
+	// Публикуем сообщение в канал
+	err = client.Publish(context.Background(), "chat:room", []byte(`{"input": "Hello, World!"}`))
+	if err != nil {
+		log.Fatalln("Publish error:", err)
+	}
+
+	// Оставляем приложение запущенным
+	select {}
 }
