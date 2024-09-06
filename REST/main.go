02e1a77438ca039bb1b45711a40169af62577196
cooler-SAI <cooler19860212@yahoo.com>
2024-09-06 20:52:57 +0300

    Go/Learning: set up base code

--- REST/main.go
+++ REST/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	_, err := fmt.Fprintf(w, "Welcome to the home page!")
+	if err != nil {
+		return
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello REST")
+	http.HandleFunc("/", homeHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
