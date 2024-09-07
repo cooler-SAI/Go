edc98b16b67ca5c0a370c717668f7efb8e261b38
cooler-SAI <cooler19860212@yahoo.com>
2024-09-07 20:19:19 +0300

    Go/Learning: heimdall - added middleware

--- heimdall/main.go
+++ heimdall/main.go
@@ -3,25 +3,56 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/gojek/heimdall/v7/httpclient"
 )
 
-func main() {
+func loggingMiddleware(next http.RoundTripper) http.RoundTripper {
+	return roundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		log.Println("Send Request:", req.URL)
 
+		res, err := next.RoundTrip(req)
+
+		if err != nil {
+			log.Println("Error Request:", err)
+		} else {
+			log.Println("Got Answer:", res.Status)
+		}
+
+		return res, err
+	})
+}
+
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (f roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req)
+}
+
+func main() {
 	timeout := 10 * time.Second
 
-	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout), httpclient.WithRetryCount(3))
+	httpClient := &http.Client{
+		Transport: loggingMiddleware(http.DefaultTransport),
+		Timeout:   timeout,
+	}
 
-	res, err := client.Get("https://api.example.com/data", nil)
+	client := httpclient.NewClient(
+		httpclient.WithHTTPClient(httpClient),
+		httpclient.WithRetryCount(3),
+	)
+
+	res, err := client.Get("https://api.github.com/repos/gojek/heimdall", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		defer func(Body io.ReadCloser) {
 			err := Body.Close()
 			if err != nil {
-
+				fmt.Println("Error closing body:", err)
 			}
 		}(res.Body)
 		fmt.Println("Status:", res.StatusCode)
