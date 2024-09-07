537abcb831bc84999fafa337fd3c02014c98b7b0
cooler-SAI <cooler19860212@yahoo.com>
2024-09-07 19:39:00 +0300

    Go/Learning: added heimdall base code

--- heimdall/main.go
+++ heimdall/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/gojek/heimdall/v7/httpclient"
+)
 
 func main() {
-	fmt.Println("Hello heimdall")
+
+	timeout := 10 * time.Second
+
+	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout), httpclient.WithRetryCount(3))
+
+	res, err := client.Get("https://api.example.com/data", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+
+			}
+		}(res.Body)
+		fmt.Println("Status:", res.StatusCode)
+	}
 }
