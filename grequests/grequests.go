09b29af5a42b1645d0ec5706ab41a415d5f5ebfd
cooler-SAI <cooler19860212@yahoo.com>
2024-09-08 12:48:09 +0300

    Go/Learning: added JSON struct

--- grequests/grequests.go
+++ grequests/grequests.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/levigross/grequests"
 )
 
+type Post struct {
+	UserID int    `json:"userId"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
 func main() {
-	resp, err := grequests.Get("https://jsonplaceholder.typicode.com/posts", nil)
+	resp, err := grequests.Get("https://jsonplaceholder.typicode.com/posts?userId=10", nil)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Ошибка:", err)
 		return
 	}
-	fmt.Println(resp.String())
+
+	var posts []Post
+	if err := json.Unmarshal(resp.Bytes(), &posts); err != nil {
+		fmt.Println("Ошибка декодирования JSON:", err)
+		return
+	}
+
+	for _, post := range posts {
+		fmt.Printf("ID: %d, Title: %s\n", post.ID, post.Title)
+	}
 }
