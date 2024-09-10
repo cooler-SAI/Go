2e371f34fe8f66c0bc33991be8d471593b70fec7
cooler-SAI <cooler19860212@yahoo.com>
2024-09-10 20:04:24 +0300

    Go/Learning: added new packages

--- graphql/go.mod
+++ graphql/go.mod
@@ -1,1 +1,11 @@
 module graphql
+
+go 1.23.1
+
+require (
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- graphql/main.go
+++ graphql/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/rs/zerolog"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/graphql-go/graphql"
+)
+
+// Определяем тип "User"
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+// Определяем корневой запрос
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"user": &graphql.Field{
+			Type: userType,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				// Простой пример, возвращаем пользователя
+				return map[string]interface{}{
+					"id":   1,
+					"name": "John Doe",
+				}, nil
+			},
+		},
+	},
+})
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		log.Printf("не удалось выполнить запрос: %+v", result.Errors)
+	}
+	return result
+}
 
 func main() {
-	fmt.Println("Hello graphql")
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	// Создаем схему
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	if err != nil {
+		log.Fatalf("не удалось создать схему: %v", err)
+	}
+
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query().Get("query")
+		result := executeQuery(query, schema)
+		err := json.NewEncoder(w).Encode(result)
+		if err != nil {
+			return
+		}
+	})
+
+	fmt.Println("Сервер запущен на порту 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
