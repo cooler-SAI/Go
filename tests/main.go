28ea12a86f202c57aab3672b7832c70b92330ba7
cooler-SAI <cooler19860212@yahoo.com>
2024-09-11 08:37:12 +0300

    Go/Learning: added more base tests

--- tests/main.go
+++ tests/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	fmt.Println("Hello Tests!")
+	a := 100
+	b := 200
+
+	fmt.Println("the result of a and b is :", IntMin(a, b))
 }
