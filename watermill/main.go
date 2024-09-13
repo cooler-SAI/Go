2017f133e569971ae5c7eb0b528c4fb2d45b1be8
cooler-SAI <cooler19860212@yahoo.com>
2024-09-13 22:28:11 +0300

    Go/Learning: added base code watermill

--- watermill/main.go
+++ watermill/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
+)
 
 func main() {
-	fmt.Println("Hello watermill")
+	// Create a logger
+	logger := watermill.NewStdLogger(false, false)
+
+	// Use GoChannel as a transport layer
+	pubSub := gochannel.NewGoChannel(gochannel.Config{}, logger)
+
+	// Publish messages
+	go func() {
+		for i := 0; i < 5; i++ {
+			msg := message.NewMessage(watermill.NewUUID(), []byte(fmt.Sprintf("Message %d", i)))
+			if err := pubSub.Publish("example_topic", msg); err != nil {
+				log.Fatalf("Failed to publish message: %v", err)
+			}
+		}
+	}()
+
+	// Create a context
+	ctx := context.Background()
+
+	// Subscribe to messages using the context
+	messages, err := pubSub.Subscribe(ctx, "example_topic")
+	if err != nil {
+		log.Fatalf("Subscription error: %v", err)
+	}
+
+	for msg := range messages {
+		fmt.Printf("Received message: %s\n", string(msg.Payload))
+		msg.Ack()
+	}
 }
