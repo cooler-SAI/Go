3bbe1bbc02f7a2f7178a5f594a2298c3f77d3fad
cooler-SAI <cooler19860212@yahoo.com>
2024-09-18 22:29:06 +0300

    Go/Learning: update clients

--- rcpx-project/client.go
+++ rcpx-project/client.go
@@ -6,22 +6,29 @@ import (
 	"github.com/smallnest/rpcx/client"
 )
 
+type Args struct {
+	A int
+	B int
+}
+
 func main() {
-	d, _ := client.NewPeer2PeerDiscovery("tcp@localhost:8972", "")
+	d, _ := client.NewPeer2PeerDiscovery("tcp@localhost:8989", "")
 	clientNew := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
 	defer func(clientNew client.XClient) {
 		err := clientNew.Close()
 		if err != nil {
-
+			fmt.Printf("Error closing client: %v\n", err)
 		}
 	}(clientNew)
 
-	args := &Args{A: 10, B: 20} // Используем структуру Args из server.go
+	args := &Args{A: 10, B: 20}
 	var reply int
+
+	// Вызываем метод "Mul"
 	err := clientNew.Call(context.Background(), "Mul", args, &reply)
 	if err != nil {
-		fmt.Printf("failed to call: %v\n", err)
+		fmt.Printf("Failed to call: %v\n", err)
+	} else {
+		fmt.Printf("Result: %d\n", reply)
 	}
-
-	fmt.Printf("Result: %d\n", reply)
 }
