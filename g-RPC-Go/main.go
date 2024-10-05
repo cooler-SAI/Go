d58adf4b757e7b1fd08e26a09463a98820dcb2d6
cooler-SAI <cooler19860212@yahoo.com>
2024-10-05 10:45:18 +0300

    Go/Learning: set up server g-RPC-Go

--- g-RPC-Go/main.go
+++ g-RPC-Go/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	"g-RPC-Go/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	proto.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(_ context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{Message: "Hello, " + in.Name}, nil
+}
 
 func main() {
-	fmt.Println("hello gopher")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Error Start the Server: %v", err)
+	}
+	s := grpc.NewServer()
+	proto.RegisterGreeterServer(s, &server{})
+	log.Println("Server run at port :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 }
