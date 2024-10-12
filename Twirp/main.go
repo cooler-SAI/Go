d012c31081319da2c9b820134accef3cab414f58
cooler-SAI <cooler19860212@yahoo.com>
2024-10-12 14:59:11 +0300

    Go/Learning: refactor project, fix up warnings

--- Twirp/main.go
+++ Twirp/main.go
@@ -9,18 +9,15 @@ import (
 	"github.com/twitchtv/twirp"
 )
 
-// Определяем реализацию сервиса Greeter
 type greeterServer struct{}
 
-// Реализация метода SayHello
-func (s *greeterServer) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
+func (s *greeterServer) SayHello(_ context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
 	return &proto.HelloResponse{
 		Message: "Hello, " + req.Name,
 	}, nil
 }
 
 func main() {
-	// Создаем сервер на основе интерфейса GreeterServer
 	server := &greeterServer{}
 	twirpHandler := proto.NewGreeterServer(server, twirp.WithServerPathPrefix("/twirp"))
 
