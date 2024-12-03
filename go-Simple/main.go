4cacd4b3e8015f7a05dfdccc0091ee0090827574
cooler-SAI <cooler19860212@yahoo.com>
2024-12-03 22:04:03 +0300

    base training

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello go-Simple")
+
+	number := 42
+	fmt.Println("The number is", number)
+
+	point := &number
+	fmt.Println("The point is", point)
+
+	*point = 100
+	fmt.Println("The changed point is", *point)
 }
