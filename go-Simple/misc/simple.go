15a0a036b98649de094008b23d3452b5a6781b3a
cooler-SAI <cooler19860212@yahoo.com>
2024-12-07 23:58:49 +0300

    added simple check

--- go-Simple/misc/simple.go
+++ go-Simple/misc/simple.go
@@ -1,1 +1,10 @@
 package misc
+
+import "math"
+
+func calculatePlinth(totalMeters float64, costPerPlinth float64) (int, float64) {
+	plinthLength := 2.7
+	plinthCount := int(math.Ceil(totalMeters / plinthLength))
+	totalCost := float64(plinthCount) * costPerPlinth
+	return plinthCount, totalCost
+}
