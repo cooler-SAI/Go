6cb6688017e268805d34abf6a701b1b9a4f2f269
cooler-SAI <cooler19860212@yahoo.com>
2024-12-08 00:05:38 +0300

    misc ups

--- go-Simple/misc/simple.go
+++ go-Simple/misc/simple.go
@@ -2,7 +2,7 @@ package misc
 
 import "math"
 
-func calculatePlinth(totalMeters float64, costPerPlinth float64) (int, float64) {
+func CalculatePlinth(totalMeters float64, costPerPlinth float64) (int, float64) {
 	plinthLength := 2.7
 	plinthCount := int(math.Ceil(totalMeters / plinthLength))
 	totalCost := float64(plinthCount) * costPerPlinth
