3257ac70911651ada9814cded1909be04f8c3c08
cooler-SAI <cooler19860212@yahoo.com>
2024-12-10 21:32:36 +0300

    added base enter app

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello World")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hello! Please enter your name: ")
+
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("Hello, %s! Welcome to GoLang programming...\n", text)
 
 }
