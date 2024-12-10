6487294c06543a097b0f109175d6e395b77cb8a0
cooler-SAI <cooler19860212@yahoo.com>
2024-12-10 20:21:02 +0300

    clean up code

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -3,41 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	/*fmt.Println("Hello go-Simple")
-
-	number := 42
-	fmt.Println("The number is", number)
-
-	point := &number
-	fmt.Println("The point is", point)
-
-	*point = 100
-	fmt.Println("The changed point is", *point)
-
-	misc.ChangeValue(point)
-	fmt.Println("The changed by func point is", *point)
-
-	fmt.Println("End of program...")
-
-	arr := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("Original array is: ", arr)
-
-	misc.ChangeArray(&arr, 500)
-	fmt.Println("Changed array is: ", arr)
-
-	misc.ChangeArrayMulti(&arr, 10)
-	fmt.Println("Changed array Multi is: ", arr)
-
-	totalMeters := 90.0
-	costPerPlinth := 850.0
-	plinthCount, totalCost := misc.CalculatePlinth(totalMeters, costPerPlinth)
-	fmt.Printf("Needs plinths: %d\n", plinthCount)
-	fmt.Printf("Total Cost: %.2f RUB\n", totalCost)
-
-	firstNumber := 2500
-	secondNumber := 5000
-	totalNum := misc.CheckResult(firstNumber, secondNumber)
-	fmt.Println("Check result:", totalNum)*/
 
 	fmt.Println("Hello World")
 
