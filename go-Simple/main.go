a01d94e3a830be9b2b80013ff5bde8cf828b86fd
cooler-SAI <cooler19860212@yahoo.com>
2024-12-20 21:15:18 +0300

    added simple func

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"go-Simple/misc"
 )
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
+	/*reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Hello! Please enter your name: ")
 
 	text, _ := reader.ReadString('\n')
-	fmt.Printf("Hello, %s! Welcome to GoLang programming...\n", text)
+	fmt.Printf("Hello, %s! Welcome to GoLang programming...\n", text)*/
+
+	numbers := []int{1, 2, 3, 4, 5}
+	sum, avg := misc.SumAndAverage(numbers)
+	fmt.Printf("Sum: %d, Avg: %.2f\n", sum, avg)
 
 }
--- go-Simple/misc/simple.go
+++ go-Simple/misc/simple.go
@@ -13,3 +13,19 @@ func CheckResult(firstNum int, secondNum int) int {
 	result := firstNum + secondNum
 	return result
 }
+
+func SumAndAverage(numbers []int) (int, float64) {
+
+	if len(numbers) == 0 {
+		return 0, 0.0
+	}
+
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+
+	average := float64(sum) / float64(len(numbers))
+
+	return sum, average
+}
