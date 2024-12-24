d631fc307a76a36546990118e1da12dee3777e27
cooler-SAI <cooler19860212@yahoo.com>
2024-12-24 23:33:22 +0300

    new func added here

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -10,4 +10,7 @@ func main() {
 	result := misc.Sum(3, 5)
 	result2 := misc.Sum(-2, 4)
 	fmt.Println(result, result2)
+
+	result3 := misc.Max(10, 20)
+	fmt.Println(result3)
 }
--- go-Simple/misc/funcs.go
+++ go-Simple/misc/funcs.go
@@ -3,3 +3,10 @@ package misc
 func Sum(a int, b int) int {
 	return a + b
 }
+
+func Max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
