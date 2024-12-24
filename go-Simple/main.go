30281745dd0952ea30ecb6f08082ad743e9ea5ea
cooler-SAI <cooler19860212@yahoo.com>
2024-12-24 22:10:40 +0300

    simple added

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -7,21 +7,6 @@ import (
 
 func main() {
 
-	/*reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Hello! Please enter your name: ")
-
-	text, _ := reader.ReadString('\n')
-	fmt.Printf("Hello, %s! Welcome to GoLang programming...\n", text)*/
-
-	/*numbers := []int{1, 2, 3, 4, 5}
-	sum, avg := misc.SumAndAverage(numbers)
-	fmt.Printf("Sum: %d, Avg: %.2f\n", sum, avg)*/
-
-	/*input := "hello"
-	reversed, uniqueCount := misc.ReverseAndCountUnique(input)
-	fmt.Printf("Reversed: %s, Unique Count: %d\n", reversed, uniqueCount)*/
-
-	inputSlice := []int{1, 2, 3, 4, 5}
-	result := misc.FilterEven(inputSlice)
+	result := misc.Sum(3, 5)
 	fmt.Println(result)
 }
---
+++ go-Simple/misc/funcs.go
@@ -0,0 +1,5 @@
+package misc
+
+func Sum(a int, b int) int {
+	return a + b
+}
