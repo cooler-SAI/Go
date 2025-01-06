b7afbc96a666634009951e897e41ff5f9d770fb4
cooler-SAI <cooler19860212@yahoo.com>
2025-01-06 20:29:07 +0300

    clean up

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"go-Simple/books"
-)
-
 func main() {
-
-	books.AddBook("The Idiot", "F.M. Dostoevsky", 1869, "Novel", true)
-	books.AddBook("Crime and Punishment", "F.M. Dostoevsky", 1866, "Novel", true)
-	books.AddBook("War and Peace", "L.N. Tolstoy", 1869, "Novel", true)
-
-	books.SearchBooks("F.M. Dostoevsky", "author")
-
-	books.DeleteBook("F.M. Dostoevsky")
-
-	books.PrintBooks()
 
 }
--- go-Simple/misc/main_archive.go
+++ go-Simple/misc/main_archive.go
@@ -40,5 +40,17 @@ func main() {
 
 	result7 := misc.PrintNTimes("Hello", 10)
 	fmt.Println(result7)
+
+    books.AddBook("The Idiot", "F.M. Dostoevsky", 1869, "Novel", true)
+	books.AddBook("Crime and Punishment", "F.M. Dostoevsky", 1866, "Novel", true)
+	books.AddBook("War and Peace", "L.N. Tolstoy", 1869, "Novel", true)
+
+	books.SearchBooks("F.M. Dostoevsky", "author")
+
+	books.DeleteBook("F.M. Dostoevsky")
+
+	books.PrintBooks()
+
+
 }
 */
