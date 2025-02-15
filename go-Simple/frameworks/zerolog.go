24d81b65fbc5d3eb73998205183290929117e5e2
cooler-SAI <cooler19860212@yahoo.com>
2025-02-15 19:45:30 +0300

    set up logger code

--- go-Simple/frameworks/zerolog.go
+++ go-Simple/frameworks/zerolog.go
@@ -1,1 +1,14 @@
 package frameworks
+
+import (
+	"github.com/rs/zerolog"
+	"os"
+)
+
+var Logger zerolog.Logger
+
+func InitLogger() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+}
