26654e862cc2d2e2cfd15e231e4c663e20a5a2f4
cooler-SAI <cooler19860212@yahoo.com>
2025-02-17 21:08:14 +0300

    result set up sum

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -6,9 +6,16 @@ func main() {
 
 	fmt.Println("Hello World")
 
-	var a = 5
-	var b = 2
+	var a = 1
+	var b = 1
 	sum := a + b
 	fmt.Println("Result: ", sum)
+
+	if sum > 5 {
+		fmt.Println("Result > 5 ")
+
+	} else {
+		fmt.Println("Result < 5 ")
+	}
 
 }
