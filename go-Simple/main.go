372f5db62e89d4ee1d3d780ec766c37410400e29
cooler-SAI <cooler19860212@yahoo.com>
2025-02-18 22:10:06 +0300

    added logger

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "go-Simple/frameworks"
+
 func main() {
+
+	frameworks.InitLogger()
+
+	frameworks.Logger.Info().Msg("Hello World")
+
+	frameworks.Logger.Error().Msg("WARNING!!!")
 
 }
