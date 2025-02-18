1b65792523d3e814505019b87a998420041fd2f7
cooler-SAI <cooler19860212@yahoo.com>
2025-02-18 21:58:27 +0300

    clean up

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go-Simple/functions"
-)
-
 func main() {
-
-	fmt.Println("Hello World")
-
-	functions.SimpleFor()
-
-	functions.SimpleMulti()
-
-	functions.SimpleSlice()
 
 }
