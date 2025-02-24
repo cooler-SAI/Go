1774024bc522c905025d594434bee3e7edb0cdf7
cooler-SAI <cooler19860212@yahoo.com>
2025-02-24 21:00:50 +0300

    simple

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"go-Simple/frameworks"
 )
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
 
 func main() {
 
 	frameworks.InitLogger()
+
+	p := Person{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+
+	fmt.Print(p)
 
 }
