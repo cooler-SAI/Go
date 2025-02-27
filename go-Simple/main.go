3af4b3a9b9fa1496d23745054f4ddd9583f741aa
cooler-SAI <cooler19860212@yahoo.com>
2025-02-27 20:55:22 +0300

    move up code

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
-func handler(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprintf(w, "Hello, World!")
-	if err != nil {
-		return
-	}
-}
 func main() {
-
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
-	}
-
+	fmt.Println("hello world")
 }
---
+++ go-Simple/misc/text_reader.go
@@ -0,0 +1,22 @@
+package misc
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello, World!")
+	if err != nil {
+		return
+	}
+}
+func main() {
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+
+}
