6d4688591270a0b91121a6aec870bd2168acf061
cooler-SAI <cooler19860212@yahoo.com>
2025-02-27 20:48:29 +0300

    added simple web

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello, World!")
+	if err != nil {
+		return
+	}
+}
 func main() {
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 
 }
