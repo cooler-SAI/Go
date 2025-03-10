65933a1b0ce341ecc950b3c952e5f6d9899fac84
cooler-SAI <coolerme86@gmail.com>
2025-03-10 22:06:25 +0300

    added reader

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,10 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 
-	fmt.Println("hello world")
-	fmt.Println("Test one here!")
+	file, err := os.Open("names.txt")
+	if err != nil {
+		fmt.Println("Can't open file, sorry...", err)
+		return
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Can't close file, sorry...", err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		name := scanner.Text()
+		if strings.HasPrefix(name, "A") {
+			fmt.Println(name)
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		_, err := fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		if err != nil {
+			return
+		}
+
+	}
 
 }
