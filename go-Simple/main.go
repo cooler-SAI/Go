742db1096beb84cd0ff2d401595e6238ac119f2c
cooler-SAI <coolerme86@gmail.com>
2025-04-01 20:35:15 +0300

    code updates

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+type Product struct {
+	ID    int
+	Name  string
+	Price float64
+}
+
+func (p *Product) Discount(percentage float64) {
+	p.Price = p.Price * (1 - percentage/100)
+}
+
 func main() {
+	product := Product{
+		ID:    1,
+		Name:  "Milk",
+		Price: 100,
+	}
+	product.Discount(10)
+	fmt.Println(product)
 
 }
