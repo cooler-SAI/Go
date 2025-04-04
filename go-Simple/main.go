b66873bcb281251b9107a897bc0f5dc654d407f0
cooler-SAI <coolerme86@gmail.com>
2025-04-04 21:58:00 +0300

    few up

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("misc up")
 
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+
+	text = strings.Trim(text, "\n")
+
+	fmt.Printf("text: %s\n", text)
 }
