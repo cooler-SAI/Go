8d583ae23f28fc90bd065d947a0f57141e58ff7c
cooler-SAI <coolerme86@gmail.com>
2025-05-27 22:40:50 +0300

    clean up

--- go-Simple/functions/funcs.go
+++ go-Simple/functions/funcs.go
@@ -133,3 +133,17 @@ func FileReader() {
 		fmt.Println("Error reading file:", err)
 	}
 }
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Swap(x, y string) (string, string) {
+	return y, x
+}
+
+func Split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
