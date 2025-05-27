1acd48148f0748af1c509ddb4df46a4f0effce6d
cooler-SAI <coolerme86@gmail.com>
2025-05-27 23:18:25 +0300

    feat: Integrate zerolog for all application output
    This commit refactors the application to use zerolog for all console
    output, including both application logs and user-facing messages.
    
    Key changes:
    - Replaced all `fmt.Print` and `fmt.Printf` calls with corresponding
      `zerolog` methods (e.g., `log.Info().Msg()`, `log.Info().Msgf()`).
    - Configured zerolog to use `zerolog.ConsoleWriter` for human-readable
      output directed to `os.Stderr`. This means user prompts and messages
      will now also appear on stderr with log formatting (timestamps, levels).
    - Enhanced error handling for reading user input: the application now
      exits if reading the user's name fails.
    - Application lifecycle events (start, finish, user interaction points)
      are now logged using structured logging provided by zerolog.

--- go-Simple/go.mod
+++ go-Simple/go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/cooler-SAI/go-Tools v0.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 )
--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	// "fmt" // fmt is no longer used for console output
+	"os"
+	"strings"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	// Configure zerolog for console output (human-readable)
+	// This logger sends output to os.Stderr
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Application started. Asking for user's name.")
+
+	// Replaced fmt.Println with zerolog for the user prompt
+	// This message will now appear on stderr with log formatting
+	log.Info().Msg("HellO! What's your name ?")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Error().Err(err).Msg("Error reading input from stdin")
+		// If reading the name fails, it's often best to exit.
+		return
+	}
+
+	name = strings.TrimSpace(name)
+
+	if name != "" {
+		log.Info().Str("userName", name).Msg("User provided a name.")
+		// Replaced fmt.Printf with zerolog's Msgf for the user greeting
+		// This message will also appear on stderr with log formatting
+		log.Info().Msgf("Good to see YOu, %s! Welcome to the world of Go!", name)
+	} else {
+		log.Warn().Msg("User did not provide a name.")
+		// Replaced fmt.Println with zerolog for the alternative user message
+		// This message will also appear on stderr with log formatting
+		log.Info().Msg("It seems you didn't introduce yourself. Well, welcome to the world of Go!")
+	}
+
+	log.Info().Msg("Application finished.")
 }
