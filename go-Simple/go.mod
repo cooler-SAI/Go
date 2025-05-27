8f93c7e2986648bce33fe1abcf1a98641523c40d
cooler-SAI <coolerme86@gmail.com>
2025-05-27 20:52:53 +0300

    clean up

--- go-Simple/go.mod
+++ go-Simple/go.mod
@@ -3,6 +3,7 @@ module go-Simple
 go 1.23
 
 require (
+	github.com/cooler-SAI/go-Tools v0.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go-Simple/reader" // Assuming the reader package is in go-Simple/reader
-	"log"
-)
-
-func greet() {
-	fmt.Println("Hello Simple")
-}
-
-func Hello() {
-	fmt.Println("Hello again")
-}
-
 func main() {
-	greet()
-	Hello()
 
-	// Reading names from the file
-	names, err := reader.Reader("names.txt")
-	if err != nil {
-		log.Fatalf("Error reading names: %v", err)
-	}
-
-	// Outputting names starting with "A"
-	fmt.Println("\nNames starting with 'A':")
-	for _, name := range names {
-		fmt.Println(name)
-	}
-
-	fmt.Println("Bye Simple")
 }
