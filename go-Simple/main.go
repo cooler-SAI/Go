00cc44e6119ece031d476130b88abadfd450826b
cooler-SAI <coolerme86@gmail.com>
2025-05-27 21:25:32 +0300

    func test up

--- go-Simple/main.go
+++ go-Simple/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
+
+	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 
 }
