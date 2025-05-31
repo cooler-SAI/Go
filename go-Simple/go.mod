07170cf6a3c58a83deb4e4da72e7e3959514f6a9
cooler-SAI <coolerme86@gmail.com>
2025-05-31 22:16:41 +0300

    added tools package

--- go-Simple/go.mod
+++ go-Simple/go.mod
@@ -1,9 +1,9 @@
 module go-Simple
 
-go 1.23
+go 1.24.3
 
 require (
-	github.com/cooler-SAI/go-Tools v0.0.1 // indirect
+	github.com/cooler-SAI/go-Tools v0.0.4 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
--- go-Simple/main.go
+++ go-Simple/main.go
@@ -2,47 +2,54 @@ package main
 
 import (
 	"bufio"
-	// "fmt" // fmt is no longer used for console output
+	"fmt" // For regular output messages (to distinguish from logs)
 	"os"
 	"strings"
 
-	"github.com/rs/zerolog"
+	// Your custom package for Zerolog configuration
+	"github.com/cooler-SAI/go-Tools/zerolog"
+
+	// Original Zerolog package for global logger access (log.Info(), log.Error(), etc.)
 	"github.com/rs/zerolog/log"
+	// Original root Zerolog package for type access (ConsoleWriter, InfoLevel)
+	zl "github.com/rs/zerolog" // Alias zerolog as zl
 )
 
 func main() {
-	// Configure zerolog for console output (human-readable)
-	// This logger sends output to os.Stderr
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	// Configure zerolog using your custom package
+	zerolog.ConfigureZerologConsoleWriter()
 
+	// Use zerolog for application lifecycle logs
 	log.Info().Msg("Application started. Asking for user's name.")
 
-	// Replaced fmt.Println with zerolog for the user prompt
-	// This message will now appear on stderr with log formatting
-	log.Info().Msg("HellO! What's your name ?")
+	// Use fmt for direct user prompts/output, as suggested
+	fmt.Println("HellO! What's your name ?")
+
+	zl.SetGlobalLevel(zl.DebugLevel)
 
 	reader := bufio.NewReader(os.Stdin)
 
 	name, err := reader.ReadString('\n')
 	if err != nil {
+		// Use zerolog for error logging
 		log.Error().Err(err).Msg("Error reading input from stdin")
-		// If reading the name fails, it's often best to exit.
-		return
+		return // Exit if there's an error reading input
 	}
 
-	name = strings.TrimSpace(name)
+	name = strings.TrimSpace(name) // Remove leading/trailing whitespace and newline character
 
 	if name != "" {
+		// Use zerolog for internal info about user input
 		log.Info().Str("userName", name).Msg("User provided a name.")
-		// Replaced fmt.Printf with zerolog's Msgf for the user greeting
-		// This message will also appear on stderr with log formatting
-		log.Info().Msgf("Good to see YOU, %s! Welcome to the world of Go!", name)
+		// Use fmt for the direct user greeting
+		fmt.Printf("Good to see YOU, %s! Welcome to the world of Go!\n", name)
 	} else {
+		// Use zerolog for warnings about user input
 		log.Warn().Msg("User did not provide a name.")
-		// Replaced fmt.Println with zerolog for the alternative user message
-		// This message will also appear on stderr with log formatting
-		log.Info().Msg("It seems you didn't introduce yourself. Well, welcome to the world of Go!")
+		// Use fmt for the alternative user message
+		fmt.Println("It seems you didn't introduce yourself. Well, welcome to the world of Go!")
 	}
 
+	// Use zerolog for application lifecycle logs
 	log.Info().Msg("Application finished.")
 }
